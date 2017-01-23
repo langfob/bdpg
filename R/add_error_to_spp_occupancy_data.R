28765a4eb24762aa04f9af2ddb88fa19b3876990
Bill Langford <bill.langford@rmit.edu.au>
2017-01-23 15:16:52 +1100

    Add add_error... and load_and_parse... from bdprobdiff

---
+++ R/add_error_to_spp_occupancy_data.R
@@ -0,0 +1,269 @@
+#===============================================================================
+
+                #  source ("add_error_to_spp_occupancy_data.R")
+
+#===============================================================================
+
+#  History
+
+#  BTL - 2015 04 30 - Created.
+
+#===============================================================================
+
+    #  Walk through the occupancy matrix (PU vs spp) and randomly 
+    #  choose to flip some of the TPs to FNs and TNs to FPs based on the 
+    #  given FP and FN error rates.
+    #  Update the occupancy matrix (bpm) as you go along, but don't update 
+    #  the PU_spp_pair_indices until you know all locations that have 
+    #  flipped.  Update PU_spp_pair_indices at the end so that you don't 
+    #  have to constantly resize this big array as you go along.
+
+add_const_error_to_spp_occupancy_data = 
+        function (bpm, FP_rates, FN_rates, num_PUs, num_spp, 
+                  random_values,   #  passing these in to make it easier to test
+                                   #  in a reproducible way
+                  bdpg_error_codes
+                  ) 
+    {
+    cat ("\nStarting add_const_error_to_spp_occupancy_data loop.\n\n")
+    
+    for (cur_spp_row in 1:num_spp)
+        {
+        for (cur_PU_col in 1:num_PUs)
+            {
+#             cat ("\n[", cur_spp_row, ",", 
+#                  cur_PU_col, 
+#                  "]", sep='')
+            if (bpm [cur_spp_row, cur_PU_col])
+                {
+                    #  TP:  This species DOES exist on this planning unit.
+                    #       Randomly choose whether to replace a given TP  
+                    #       with a false negative (FN)
+                    #       i.e., simulate not detecting that spp on that PU.                    
+                if (random_values [cur_spp_row, cur_PU_col] < FN_rates [cur_spp_row, cur_PU_col])
+                    bpm [cur_spp_row, cur_PU_col] = 0
+                
+                }  else 
+                {
+                    #  TN:  This species does NOT exist on this planning unit.
+                    #       Randomly choose whether to replace a given TN  
+                    #       with a false positive (FP).                
+                if (random_values [cur_spp_row, cur_PU_col] < FP_rates [cur_spp_row, cur_PU_col])
+                    bpm [cur_spp_row, cur_PU_col] = 1
+                
+               }  #  end else - TN so set FP                
+            }  #  end for - all PU cols
+        }  #  end for - all spp rows
+
+    return (bpm)
+    }
+
+#===============================================================================
+
+set_const_FP_and_FN_rates = function (parameters, bdpg_error_codes)
+    {
+        #----------------------------
+        #  Set False Positive rate.
+        #----------------------------
+    
+    spp_occ_FP_error_type = parameters$spp_occ_FP_error_type
+    
+    FP_const_rate = NA
+    if (spp_occ_FP_error_type == "CONSTANT")
+        {
+        FP_const_rate = parameters$spp_occ_FP_const_rate
+        
+        } else if (spp_occ_FP_error_type == "RANDOM_UNIFORM_CONSTANT")
+        {
+        lower_bound = parameters$spp_occ_FP_rate_lower_bound
+        upper_bound = parameters$spp_occ_FP_rate_upper_bound
+            
+        FP_const_rate = runif (1, min=lower_bound, max=upper_bound)
+            
+        } else  #  unknown type of error to add
+        {
+        cat ("\n\nERROR: Unknown spp_occ_FP_error_type = '", 
+             spp_occ_FP_error_type, "'.\n", sep='')
+        quit (save="no", bdpg_error_codes$ERROR_STATUS_unknown_spp_occ_FP_error_type)
+        }
+    
+        #----------------------------
+        #  Set False Negative rate.
+        #----------------------------
+    
+    spp_occ_FN_error_type = parameters$spp_occ_FN_error_type
+    
+    FN_const_rate = NA
+    if (spp_occ_FN_error_type == "CONSTANT")
+        {
+        FN_const_rate = parameters$spp_occ_FN_const_rate
+        
+        } else if (spp_occ_FN_error_type == "RANDOM_UNIFORM_CONSTANT")
+        {
+        lower_bound = parameters$spp_occ_FN_rate_lower_bound
+        upper_bound = parameters$spp_occ_FN_rate_upper_bound
+            
+        FN_const_rate = runif (1, min=lower_bound, max=upper_bound)
+            
+        } else  #  unknown type of error to add
+        {
+        cat ("\n\nERROR: Unknown spp_occ_FN_error_type = '", 
+             spp_occ_FN_error_type, "'.\n", sep='')
+        quit (save="no", bdpg_error_codes$ERROR_STATUS_unknown_spp_occ_FN_error_type)
+        }
+    
+    #--------------------
+
+    return (list (FP_const_rate = FP_const_rate, 
+                  FN_const_rate = FN_const_rate))
+    }
+
+#===============================================================================
+
+    #  Usually, the number of TNs and TPs will be unbalanced.
+    #  Therefore, there will be far more opportunities to inject 
+    #  FPs than FNs or vice versa.  
+    #  Consequently, even if the FP and FN rates are set to the  
+    #  same value, there are likely to be far more FPs than FNs  
+    #  or vice versa in the apparent matrix.
+    #  If you want to keep the opportunities for each of them 
+    #  to be more balanced, then you can multiply the dominant 
+    #  one by the lesser one's fraction of occurrence and 
+    #  reset the rate for the dominant so that both end up 
+    #  with the same counts.
+    #
+    #  Example: if there are 
+    #       - 100 entries total 
+    #       - 70 TNs
+    #       - 30 TPs 
+    #  and you want 0.1 probability of FN, then you should get 
+    #  approximately 3 FNs.  If you want the FPs to match the FNs,  
+    #  then x * 70 FPs must equal 3 FPs too.  
+    #  So, the multiplier x = 3 / 70 ~ 0.0429
+    #  i.e., the adjusted_P(FP) = num_FNs / num_TNs
+        
+match_FP_and_FN_counts_to_smaller_of_the_two = function (num_TPs, num_TNs, 
+                                      FP_const_rate, FN_const_rate)
+    {
+    approx_num_FNs = round (FN_const_rate * num_TPs)    
+    approx_num_FPs = round (FP_const_rate * num_TNs)
+
+    cat ("\n\nBefore matching of FP and FN const_rates:", 
+         "\n\tnum_TPs = ", num_TPs, 
+         "\n\tnum_TNs = ", num_TNs, 
+         "\n\tapprox_num_FNs = ", approx_num_FNs, 
+         "\n\tapprox_num_FPs = ", approx_num_FPs, 
+         "\n")
+
+    if ((num_TNs > 0) & (num_TPs > 0))
+        {
+        if (num_TNs > num_TPs)
+            {
+            FP_const_rate = approx_num_FNs / num_TNs 
+            
+            } else 
+            {
+            FN_const_rate = approx_num_FPs / num_TPs            
+            }        
+        } else
+        {
+        cat ("\n\nNot matching FP and FN counts since num_TNs or num_TPs = 0.\n")
+        }
+    
+    approx_num_FNs = round (FN_const_rate * num_TPs)    
+    approx_num_FPs = round (FP_const_rate * num_TNs)
+
+    cat ("\n\nAfter matching of FP and FN const_rates:", 
+         "\n\tFP_const_rate = ", FP_const_rate,
+         "\n\tFN_const_rate = ", FN_const_rate,
+         "\n\tapprox_num_FNs = ", approx_num_FNs, 
+         "\n\tapprox_num_FPs = ", approx_num_FPs, 
+         "\n")
+
+
+    return (list (FP_const_rate = FP_const_rate,
+                  FN_const_rate = FN_const_rate))
+    }
+
+#===============================================================================
+
+add_error_to_spp_occupancy_data = 
+        function (parameters, bpm, num_PU_spp_pairs, num_PUs, num_spp, 
+                  bdpg_error_codes) 
+    {
+    FP_and_FN_const_rates = set_const_FP_and_FN_rates (parameters, 
+                                                       bdpg_error_codes)
+    
+    FP_const_rate = FP_and_FN_const_rates$FP_const_rate
+    FN_const_rate = FP_and_FN_const_rates$FN_const_rate
+    
+    match_error_counts = FALSE
+    if (! is.null (parameters$match_error_counts))
+        match_error_counts = parameters$match_error_counts
+        
+    if (match_error_counts)
+        {
+        num_TPs = sum (bpm)
+        num_TNs = length (bpm) - num_TPs        
+
+        FP_FN_const_rate_pair = 
+            match_FP_and_FN_counts_to_smaller_of_the_two (num_TPs, num_TNs, 
+                                                          FP_const_rate, 
+                                                          FN_const_rate)
+        
+        FP_const_rate = FP_FN_const_rate_pair$FP_const_rate
+        FN_const_rate = FP_FN_const_rate_pair$FN_const_rate
+        }
+    
+    FP_rates = matrix (rep (FP_const_rate, (num_PUs * num_spp)), 
+                        nrow=num_spp,
+                        ncol=num_PUs,
+                        byrow=TRUE)
+    
+    FN_rates = matrix (rep (FN_const_rate, (num_PUs * num_spp)), 
+                        nrow=num_spp,
+                        ncol=num_PUs,
+                        byrow=TRUE)
+    
+    random_values = matrix (runif (num_PUs * num_spp), 
+                            nrow=num_spp,
+                            ncol=num_PUs,
+                            byrow=TRUE)
+    
+    app_spp_occupancy_data = 
+        add_const_error_to_spp_occupancy_data (bpm, 
+                                                FP_rates, FN_rates, 
+                                                num_PUs, num_spp, 
+                                                random_values, 
+                                               bdpg_error_codes)
+    
+    app_PU_spp_pair_indices = 
+        build_PU_spp_pair_indices_from_occ_matrix (app_spp_occupancy_data, 
+                                                    num_PUs, num_spp)
+
+    app_num_spp = length (unique (app_PU_spp_pair_indices [,"spp_ID"]))
+    app_num_PUs = length (unique (app_PU_spp_pair_indices [,"PU_ID"]))
+    
+    if (app_num_spp != num_spp) 
+        cat ("\n\nAfter adding error:  app_num_spp (", app_num_spp, 
+             ") now differs from original num_spp (", num_spp, 
+             ").")
+    
+    if (app_num_PUs != num_PUs) 
+        cat ("\n\nAfter adding error:  app_num_PUs (", app_num_PUs, 
+             ") now differs from original num_PUs (", num_PUs, 
+             ").")
+    
+    return (list (original_FP_const_rate = FP_and_FN_const_rates$FP_const_rate, 
+                  original_FN_const_rate = FP_and_FN_const_rates$FN_const_rate, 
+                  match_error_counts = match_error_counts,
+                  FP_const_rate = FP_const_rate, 
+                  FN_const_rate = FN_const_rate, 
+                  app_PU_spp_pair_indices = app_PU_spp_pair_indices, 
+                  app_spp_occupancy_data = app_spp_occupancy_data, 
+                  app_num_spp = app_num_spp, 
+                  app_num_PUs = app_num_PUs))
+    }
+
+#===============================================================================
+
---
+++ R/load_and_parse_Xu_set_cover_problem_file.R
@@ -0,0 +1,248 @@
+#==============================================================================
+
+                #  load_and_parse_Xu_set_cover_problem_file.R  
+
+#  This routine reads the given input file which is assumed to contain 
+#  one of Xu's minimum set cover problems in the format specified on 
+#  his related web page at:
+#       http://www.nlsde.buaa.edu.cn/~kexu/benchmarks/set-benchmarks.htm
+
+#==============================================================================
+
+#  2016 06 12 - BTL
+#
+#  NOTE:  Xu files that you read in DO NOT provide the IDs of nodes in the 
+#         solution set, i.e., the dependent_node_IDs.  The only thing that 
+#         you know is the correct solution cost (i.e., number of PUs) and 
+#         even that is specified on one of Xu's web pages, not in the file 
+#         being read in.  I think that this is because these problems were 
+#         defined as test problems to be used in blind tests of algorithms.
+#
+#         An IMPORTANT SIDE EFFECT of this is that it means we can't wrap 
+#         another problem around them because we don't know the dependent IDs.
+#
+#==============================================================================
+
+#  FILE FORMAT:
+
+#  The file format has a 1 line header followed by 1 line for each vertex. 
+#  Each vertex line lists all of the edges assigned to a given vertex.
+
+#  Header line
+#  The header line begins with "p set " and then gives the highest edge number 
+#  in the file, followed by a space and then the number of vertices.  
+#  For example:
+#       p set 17827 450
+
+#  Vertex lines
+#  - No ID is given for the vertices, just the list of edge IDs, but 
+#  - Each vertex line begins with "s".
+#  - All entries on the line are 
+#  separated by spaces.  Each line can be of any length, so they essentially 
+#  make up a ragged array.  
+
+#  Example of first few lines of a file (with R comment markers prepended as 
+#  "#  " to allow these lines to appear in this R source file:
+#  p set 174 3
+#  s 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+#  s 1 81 82 83 84 85 86 87 88 89
+#  s 2 81 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174
+
+#-------------------------------------------------------------------------------
+
+#  Quote from Xu's page cited above explaining the format:
+#
+#  As far as we know, there is no standard format to express the minimum set 
+#  covering and maximum set packing problems. 
+#  Based on the ASCII DIMACS graph format, we propose a format for these 
+#  two problems as follows:
+# 
+#     There are 0 or more lines starting with c at the top of the file which 
+#     are comment lines and can be ignored.
+# 
+#     Following the comment lines, there is a line with the form "p set U S"  
+#     where U and S are respectively 
+#       - the number of elements in the universe (denoted by natural numbers)
+#           [BTL: these are the species IDs in biodivprobgen]
+#       - and the number of subsets in the collection.
+#           [BTL: these are the planning unit IDs in biodivprobgen]
+# 
+#     The remaining of the file is a list of lines starting with s which 
+#     indicate the subsets in the collection (e.g. the line "s 1 2 4" 
+#     indicates that there is a subset with three elements which are 1, 2 
+#     and 4).
+
+#-------------------------------------------------------------------------------
+
+#  Here are the benchmarks and with their Minimum Set Cover sizes for the 
+#  correct answer from the web page.  I've downloaded and unzipped all of these 
+#  in:
+#      /Users/bill/Desktop/Papers downloaded/Problem difficulty/Problem difficulty datasets/Xu - problem difficulty datasets.
+#  I've also copied them into:
+#      /Users/bill/D/Projects/ProblemDifficulty/data/Xu - problem difficulty datasets.
+#
+#    frb30-15-msc.tar.gz (  419 KB):   450 subsets  - 5 instances, with the size of the MSC =   420 and the size of the MSP = 30
+#    frb35-17-msc.tar.gz (  666 KB):   595 subsets  - 5 instances, with the size of the MSC =   560 and the size of the MSP = 35
+#    frb40-19-msc.tar.gz (  990 KB):   760 subsets  - 5 instances, with the size of the MSC =   720 and the size of the MSP = 40
+#    frb45-21-msc.tar.gz (1412 KB):   945 subsets  - 5 instances, with the size of the MSC =   900 and the size of the MSP = 45
+#    frb50-23-msc.tar.gz (1943 KB): 1150 subsets  - 5 instances, with the size of the MSC = 1100 and the size of the MSP = 50
+#    frb53-24-msc.tar.gz (2281 KB): 1272 subsets  - 5 instances, with the size of the MSC = 1219 and the size of the MSP = 53
+#    frb56-25-msc.tar.gz (2677 KB): 1400 subsets  - 5 instances, with the size of the MSC = 1344 and the size of the MSP = 56
+#    frb59-26-msc.tar.gz (3110 KB): 1534 subsets  - 5 instances, with the size of the MSC = 1475 and the size of the MSP = 59
+
+#==============================================================================
+
+#  History:
+
+#  2015 04 26 - BTL
+#  Cloned from /Users/bill/D/rdv-framework-old-sourceforge/framework/R/
+#               scan.ragged.array.of.integers.from.file.R
+
+#  8/23/07 - BTL
+#  Created scan.ragged.array.of.integers.from.file.R
+#      Basically just hacked around with a few lines of Daniel's code from 
+#      calc.evaluation.functions.v5.0.R 
+
+#==============================================================================
+
+load_and_parse_Xu_set_cover_problem_file <- function (infile_name)
+    {
+    cat ("\n\nIn load_and_parse_Xu_set_cover_problem_file()",
+         "\n\tinfile_name = '", infile_name, "'\n", sep='')
+
+        #  I don't really get the "" argument here, but it works.
+        #  It seems to be telling the scan routine that the data is
+        #  character data.  I tried lots of other things like "integer",
+        #  but nothing seemed to work right.  Daniel had "complex" in
+        #  there and it worked, but for no apparent reason, i.e., the
+        #  data is integer, not complex.  
+    vector_of_input_line_strings <- scan ( infile_name, "", sep = "\n" );
+    num_vertex_lines = length (vector_of_input_line_strings) - 1
+    
+    header_line_as_list_of_strings <-
+        strsplit (vector_of_input_line_strings [1], " ") [[1]];
+    
+    cat ("\n\nXu input file header values = ")
+    print (header_line_as_list_of_strings)
+    
+    max_edge_ID = as.integer (header_line_as_list_of_strings [3])
+    num_vertices = as.integer (header_line_as_list_of_strings [4])
+    
+    if (num_vertex_lines != num_vertices)
+        {
+        cat ("\n\nERROR in load_and_parse_Xu_set_cover_problem_file():",
+             "\n\tnum_vertex_lines=", num_vertex_lines, " must match ", 
+             "num_vertices=", num_vertices, 
+             "\n\n", sep='')
+        stop ()
+        }
+    
+    xu_list_of_vectors_of_edge_IDs <- vector ("list", num_vertices);
+    
+    for (i in 1:num_vertices)
+        {
+            #  Split out the various substrings of the current line.
+            #  I tried to make it so that it could split on something
+            #  other than spaces (e.g., space, tab, and comma), but
+            #  I could only get it to work with a single thing, spaces.
+            #  It looked like it should be able to take regular expressions
+            #  instead of just " ", but I couldn't get it to work.
+            #  I may have just been putting the expression in wrong though...
+        cur_line_as_list_of_strings <-
+            strsplit (vector_of_input_line_strings [i+1], " ")
+        
+            #  strsplit() returns a list with one element, i.e., 
+            #  a vector of substrings making up the line.
+            #  For example:
+            #       [[1]]
+            #        [1] "s"  "1"  "2"  "3"  "4" 
+            #
+            #  So, strip off the leading "s" and convert values to a vector 
+            #  of integers instead of a single element list containing a 
+            #  vector of strings.
+        xu_list_of_vectors_of_edge_IDs [[i]] = 
+            as.integer (cur_line_as_list_of_strings [[1]][-1])
+        }
+    
+    return (list (xu_list_of_vectors_of_edge_IDs=xu_list_of_vectors_of_edge_IDs, 
+                  num_PUs=num_vertices,
+                  num_spp=max_edge_ID))
+    }
+
+#==============================================================================
+
+load_Xu_problem_from_Xu_file_into_PU_spp_pair_indices = 
+        function (xu_list_of_vectors_of_edge_IDs,
+                  num_PUs)
+    {
+    lengths_of_vectors = lapply (xu_list_of_vectors_of_edge_IDs, length)
+    num_PU_spp_pairs = sum (unlist (lengths_of_vectors))
+    
+    PU_spp_pair_indices = data.frame (PU_ID = rep (NA, num_PU_spp_pairs),
+                                      spp_ID = rep (NA, num_PU_spp_pairs))
+        
+    cur_PU_spp_pair_ct = 0
+    for (cur_vertex_ID in seq (1, num_PUs))
+        {
+        for (cur_edge_ID in xu_list_of_vectors_of_edge_IDs [[cur_vertex_ID]])
+            {
+            cur_PU_spp_pair_ct = cur_PU_spp_pair_ct + 1
+            PU_spp_pair_indices [cur_PU_spp_pair_ct, ] = 
+                                                c (cur_vertex_ID, cur_edge_ID)
+            }
+        }
+
+    return (PU_spp_pair_indices)
+    }
+
+#==============================================================================
+
+#load_Xu_problem_from_file_into_PU_spp_pair_indices_sextet = 
+load_Xu_problem_from_Xu_file = 
+        function (input_file_name, 
+                  correct_solution_cost)
+    {
+    parsed_Xu_file_triple = 
+        load_and_parse_Xu_set_cover_problem_file (infile_name)
+    
+    xu_list_of_vectors_of_edge_IDs = 
+        parsed_Xu_file_triple$xu_list_of_vectors_of_edge_IDs
+    num_PUs = parsed_Xu_file_triple$num_PUs
+    num_spp = parsed_Xu_file_triple$num_spp
+    
+    PU_spp_pair_indices = 
+        load_Xu_problem_from_Xu_file_into_PU_spp_pair_indices (xu_list_of_vectors_of_edge_IDs,
+                                                               num_PUs)
+    
+    cat ("\n\nPU_spp_pair_indices = \n")
+    print (head (PU_spp_pair_indices))
+    cat ("\n...\n")
+    
+    cat ("\n")
+    print (tail (PU_spp_pair_indices))
+    
+    PU_col_name = names (PU_spp_pair_indices)[1]
+    spp_col_name = names (PU_spp_pair_indices)[2]
+    
+    PU_spp_pair_info <- new ("PU_spp_pair_info")
+    
+    PU_spp_pair_info@PU_spp_pair_indices <- PU_spp_pair_indices
+    PU_spp_pair_info@PU_col_name <- PU_col_name
+    PU_spp_pair_info@spp_col_name <- spp_col_name
+    PU_spp_pair_info@num_PUs <- num_PUs
+    PU_spp_pair_info@num_spp <- num_spp
+    PU_spp_pair_info@correct_solution_cost <- correct_solution_cost
+
+    return (PU_spp_pair_info)
+        
+    # return (list (PU_spp_pair_indices=PU_spp_pair_indices, 
+    #               PU_col_name=PU_col_name,
+    #               spp_col_name=spp_col_name, 
+    #               num_PUs=num_PUs,
+    #               num_spp=num_spp, 
+    #               correct_solution_cost = correct_solution_cost
+    #               ))
+    }
+
+#==============================================================================
+
