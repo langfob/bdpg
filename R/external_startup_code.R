3be5cdca5cdda8fbff6949074c9b64f859bcc4c4
Bill Langford <bill.langford@rmit.edu.au>
2017-01-23 13:32:28 +1100

    Comment out most of what's left of external_startup_code.R from bdprobdiff
    Half of this file was startup code that has been moved into other files.
    
    The other half of this file is a list of source() calls to load nearly all
    of the code for the project.  These are all being replaced with simply
    including the files in the directory of the new package, since that will
    automatically force them to be sourced when the package is built.
    One issue with this is that all of the files have to be checked to make
    sure that they do nothing other than defining functions.  For example,
    some of them have a bit of testing code in them that was possible to run
    at source time, though most of those bits were inside "if(FALSE)" protections.
    Need to pull all those out and put them into proper tests in the package's
    test directory.

--- R/external_startup_code.R
+++ R/external_startup_code.R
@@ -27,33 +27,32 @@
 
 #===============================================================================
 
-source (paste0 (sourceCodeLocationWithSlash, "BDProb.R"))
-
-source (paste0 (sourceCodeLocationWithSlash, "biodivprobgen_utilities.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gen_bdprob.R"))
-source (paste0 (sourceCodeLocationWithSlash, "add_error_to_spp_occupancy_data.R"))
-source (paste0 (sourceCodeLocationWithSlash, "testing_bdpg_functions.R"))
-source (paste0 (sourceCodeLocationWithSlash, "load_and_parse_Xu_set_cover_problem_file.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_5_derive_control_parameters.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_6_create_data_structures.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_8_link_nodes_within_groups.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_9_link_nodes_between_groups.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_9a_create_Xu_graph.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_10a_clean_up_completed_graph_structures.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_10b_compute_solution_rep_levels_and_costs.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_10c_build_adj_and_cooccurrence_matrices.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_11_summarize_and_plot_graph_structure_information.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_11aa_write_abbreviated_results_to_files.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_11a_network_measures_using_bipartite_package.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_11b_network_measures_using_igraph_package.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_12_write_network_to_marxan_files.R"))
-source (paste0 (sourceCodeLocationWithSlash, "compute_solution_scores.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_13_write_marxan_control_file_and_run_marxan.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_14_read_marxan_output_files.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gscp_15_create_master_output_structure.R"))
-
-source (paste0 (sourceCodeLocationWithSlash, "do_graph_and_marxan_analysis.R"))
-source (paste0 (sourceCodeLocationWithSlash, "gen_lognormal_overlay.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "BDProb.R"))
+#
+# source (paste0 (sourceCodeLocationWithSlash, "gen_bdprob.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "add_error_to_spp_occupancy_data.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "testing_bdpg_functions.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "load_and_parse_Xu_set_cover_problem_file.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_5_derive_control_parameters.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_6_create_data_structures.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_8_link_nodes_within_groups.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_9_link_nodes_between_groups.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_9a_create_Xu_graph.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_10a_clean_up_completed_graph_structures.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_10b_compute_solution_rep_levels_and_costs.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_10c_build_adj_and_cooccurrence_matrices.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_11_summarize_and_plot_graph_structure_information.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_11aa_write_abbreviated_results_to_files.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_11a_network_measures_using_bipartite_package.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_11b_network_measures_using_igraph_package.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_12_write_network_to_marxan_files.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "compute_solution_scores.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_13_write_marxan_control_file_and_run_marxan.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_14_read_marxan_output_files.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gscp_15_create_master_output_structure.R"))
+#
+# source (paste0 (sourceCodeLocationWithSlash, "do_graph_and_marxan_analysis.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "gen_lognormal_overlay.R"))
 
 #===============================================================================
     #  Code in this section is not just function definitions.
@@ -62,17 +61,17 @@ source (paste0 (sourceCodeLocationWithSlash, "gen_lognormal_overlay.R"))
     #  into testing conventions.
 #===============================================================================
 
-run_ID = parameters$run_id
-runset_name = parameters$runset_name
+# run_ID = parameters$run_id
+# runset_name = parameters$runset_name
 
 #===============================================================================
 
     #  Set random seed to help reproducibility.
     #  Has to be done after startup code that loads parameters structure.
-set.seed (parameters$seed)
+# set.seed (parameters$seed)
 
 #===============================================================================
 
-source (paste0 (sourceCodeLocationWithSlash, "testing_bdpg.R"))
+# source (paste0 (sourceCodeLocationWithSlash, "testing_bdpg.R"))
 
 #===============================================================================
