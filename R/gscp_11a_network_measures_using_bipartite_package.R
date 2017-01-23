6e66bf4e37a1a188133a58e3b29b8d98dfe2394d
Bill Langford <bill.langford@rmit.edu.au>
2017-01-23 14:57:28 +1100

    Add gscp_11,11a,11aa from bdprobdiff

---
+++ R/gscp_11a_network_measures_using_bipartite_package.R
@@ -0,0 +1,101 @@
+#===============================================================================
+
+            #  gscp_11a_network_measures_using_bipartite_package.R
+
+#===============================================================================
+
+                    #  Code for using bipartite package.
+
+#===============================================================================
+
+compute_network_measures_using_bipartite_package = function (bpm) 
+    {
+    cat ("\n\nAbout to create all_except_slow_indices.")
+    
+    all_except_slow_indices <- 
+      c(
+          "number of species", 
+          "connectance", 
+          "web asymmetry", 
+          "links per species", 
+    #           "number of compartments",           #  #6a slowest ("calls a recursive and hence relatively slow algorithm.") 
+    #           "compartment diversity",            #  #6b slowest ("calls a recursive and hence relatively slow algorithm.")
+          "cluster coefficient", 
+    #            "degree distribution",              #  #3 slowest ("somewhat time consuming")
+          "mean number of shared partners", 
+          "togetherness", 
+          "C score", 
+          "V ratio", 
+    #            "discrepancy",                      #  #7c slowest ("excluding discrepancy can also moderately decrease computation time")
+    #            "nestedness",                       #  #5a slowest ("not the fastest of routines")
+    #            "weighted nestedness",              #  #5b slowest ("not the fastest of routines")
+          "ISA", 
+          "SA", 
+    #            "extinction slope",                 #  #1a slowest
+    #            "robustness",                       #  #1b slowest 
+          "niche overlap", 
+    #            "weighted cluster coefficient",     #  #2 slowest
+          "weighted NODF", 
+          "partner diversity", 
+          "generality", 
+          "vulnerability", 
+          "linkage density", 
+          "weighted connectance", 
+    #            "Fisher alpha",                     #  #4 slowest ("computed iteratively and hence time consuming")
+          "interaction evenness", 
+          "Alatalo interaction evenness", 
+          "Shannon diversity", 
+          "functional complementarity" 
+    )
+    #            , "H2                                 #  #7a slowest ("require an iterative, heuristic search algorithm", #7b - same for specialisation asymmetry but it's not in this list)
+    #        )
+    
+    
+      bipartite_metrics_from_bipartite_package <- 
+    # #         t (networklevel (bpm, index=c ("links per species",
+    # #                                      "H2",
+    # #                                      "interaction evenness")))
+      
+                  #  BTL - 2015 01 07
+                  #  On a small network, ALLBUTDD took a miniscule amount 
+                  #  of extra time compared to all_except_slow_indices, 
+                  #  so I'm going to use ALLBUTDD for now.  If bigger 
+                  #  networks show this is too slow, then can revert to 
+                  #  using all_except_slow_indices.
+    #         t (networklevel (bpm, index=all_except_slow_indices))
+          t (networklevel (bpm, index="ALLBUTDD"))
+    
+    cat ("\n\nbipartite_metrics_from_bipartite_package = \n")
+    print (bipartite_metrics_from_bipartite_package)
+    
+      #  Clean up metric names...
+      #  
+      #  The bipartite metrics output has spaces in some of the metric names. 
+      #  This can cause problems sometimes for downstream uses of the output, so 
+      #  you need to replace the spaces with something other than white space.  
+      #  Here, I'll replace them with underscores.  Some of the metrics already 
+      #  use periods instead of spaces, but I won't mess with those for now in 
+      #  case it helps match them up with some other expectation from a paper or 
+      #  something.
+    
+    metrics_col_names = colnames (bipartite_metrics_from_bipartite_package)  
+    cat ("\n\nmetrics_col_names = \n")
+    print (metrics_col_names)
+    
+    new_metrics_col_names = 
+      str_replace_all (string=metrics_col_names, pattern=" ", repl="_")
+    cat ("\n\nnew_metrics_col_names = \n")
+    print (new_metrics_col_names)
+    cat ("\n\n")
+    
+    colnames (bipartite_metrics_from_bipartite_package) = new_metrics_col_names
+    
+    cat ("\n\nbipartite_metrics_from_bipartite_package with spaces removed from column names = \n")
+    print (bipartite_metrics_from_bipartite_package)
+    cat ("\n\n")
+
+    return (bipartite_metrics_from_bipartite_package)
+    }
+
+#===============================================================================
+
---
+++ R/gscp_11aa_write_abbreviated_results_to_files.R
@@ -0,0 +1,204 @@
+#===============================================================================
+
+            #  gscp_11aa_write_abbreviated_results_to_files.R
+
+#===============================================================================
+
+write_abbreviated_results_to_files = 
+            function (cur_result_row, 
+                      parameters, 
+                      num_PUs, 
+                      num_spp, 
+                      n__num_groups, 
+                      alpha__, 
+                      p__prop_of_links_between_groups, 
+                      r__density, 
+                      num_nodes_per_group, 
+                      tot_num_nodes, 
+                      num_independent_set_nodes, 
+                      num_dependent_set_nodes, 
+                      num_rounds_of_linking_between_groups, 
+                      target_num_links_between_2_groups_per_round, 
+                      num_links_within_one_group, 
+                      tot_num_links_inside_groups, 
+                      max_possible_num_links_between_groups, 
+                      max_possible_tot_num_links) 
+    {
+    all_output_col_names = 
+        c (
+            "runset_abbrev",
+            "run_ID", 
+            
+            "exceeded_thresh_for_num_spp", 
+            
+                #  Derived network parameters (after running Xu algorithm)
+            "num_PUs",
+            "num_spp",
+            "num_spp_per_PU",
+            
+            "seed",
+            
+                #  Xu input parameters
+            "n__num_groups",
+            "alpha__",
+            "p__prop_of_links_between_groups",
+            "r__density",
+            
+                #  Correct answers
+            "opt_solution_as_frac_of_tot_num_nodes",
+            "cor_num_patches",
+            
+                #  Marxan results
+            "marxan_best_num_patches",
+            "abs_marxan_best_solution_cost_err_frac",
+            "marxan_best_solution_cost_err_frac",
+            "spp_rep_shortfall",
+            "marxan_best_solution_NUM_spp_covered",
+            "marxan_best_solution_FRAC_spp_covered",
+            
+                #  Derived input parameters (before running Xu algorithm)
+            "num_nodes_per_group",
+            "tot_num_nodes",
+            "num_independent_set_nodes",
+            "num_dependent_set_nodes",
+            "num_rounds_of_linking_between_groups",
+            "target_num_links_between_2_groups_per_round",
+            "num_links_within_one_group",
+            "tot_num_links_inside_groups",
+            "max_possible_num_links_between_groups",
+            "max_possible_tot_num_links",
+            
+                #  Marxan input parameters
+            "marxan_spf_const",
+            "marxan_PROP",
+            "marxan_RANDSEED",
+            "marxan_NUMREPS",
+            "marxan_NUMITNS",
+            "marxan_STARTTEMP",
+            "marxan_NUMTEMP",
+            "marxan_COSTTHRESH",
+            "marxan_THRESHPEN1",
+            "marxan_THRESHPEN2",
+            "marxan_RUNMODE",
+            "marxan_MISSLEVEL",
+            "marxan_ITIMPTYPE",
+            "marxan_HEURTYPE",
+            "marxan_CLUMPTYPE",
+            
+            "runset_name",
+            
+                #  Bipartite measures
+            "connectance",
+            "web_asymmetry",
+            "links_per_species",
+            "number_of_compartments",
+            "compartment_diversity",
+            "cluster_coefficient",
+            "nestedness",
+            "weighted_nestedness",
+            "weighted_NODF",
+            "interaction_strength_asymmetry",
+            "specialisation_asymmetry",
+            "linkage_density",
+            "weighted_connectance",
+            "Fisher_alpha",
+            "Shannon_diversity",
+            "interaction_evenness",
+            "Alatalo_interaction_evenness",
+            "H2","number.of.species.HL",
+            "number.of.species.LL",
+            "mean.number.of.shared.partners.HL",
+            "mean.number.of.shared.partners.LL",
+            "cluster.coefficient.HL",
+            "cluster.coefficient.LL",
+            "weighted.cluster.coefficient.HL",
+            "weighted.cluster.coefficient.LL",
+            "niche.overlap.HL",
+            "niche.overlap.LL",
+            "togetherness.HL",
+            "togetherness.LL",
+            "C.score.HL",
+            "C.score.LL",
+            "V.ratio.HL",
+            "V.ratio.LL",
+            "discrepancy.HL",
+            "discrepancy.LL",
+            "extinction.slope.HL",
+            "extinction.slope.LL",
+            "robustness.HL",
+            "robustness.LL",
+            "functional.complementarity.HL",
+            "functional.complementarity.LL",
+            "partner.diversity.HL",
+            "partner.diversity.LL",
+            "generality.HL",
+            "vulnerability.LL",
+            
+                #  igraph Latapy measures
+            "ig_top",
+            "ig_bottom",
+            "ig_num_edges_m",
+            "ig_ktop",
+            "ig_kbottom",
+            "ig_bidens",
+            "ig_lcctop",
+            "ig_lccbottom",
+            "ig_distop",
+            "ig_disbottom",
+            "ig_cctop",
+            "ig_ccbottom",
+            "ig_cclowdottop",
+            "ig_cclowdotbottom",
+            "ig_cctopdottop",
+            "ig_cctopdotbottom",
+            "ig_mean_bottom_bg_redundancy",
+            "ig_median_bottom_bg_redundancy",
+            "ig_mean_top_bg_redundancy",
+            "ig_median_top_bg_redundancy"
+            )
+    
+    
+    all_output_initialized_cols = rep (NA, length (all_output_col_names))
+    
+    initialized_output_df = as.data.frame (t (all_output_initialized_cols))
+    names (initialized_output_df) = all_output_col_names
+    
+    initialized_output_df$runset_abbrev [cur_result_row]                                    = parameters$runset_name    #  parameters$runset_abbrev
+    
+    initialized_output_df$exceeded_thresh_for_num_spp                                       = TRUE
+    
+    initialized_output_df$num_PUs [cur_result_row]                                          = num_PUs
+    initialized_output_df$num_spp [cur_result_row]                                          = num_spp
+    initialized_output_df$num_spp_per_PU [cur_result_row]                                   = num_spp / num_PUs
+    initialized_output_df$seed [cur_result_row]                                             = parameters$seed
+    
+        #  Xu options
+    initialized_output_df$n__num_groups [cur_result_row]                                   = n__num_groups
+    initialized_output_df$alpha__ [cur_result_row]                                          = alpha__
+    initialized_output_df$p__prop_of_links_between_groups [cur_result_row]                 = p__prop_of_links_between_groups
+    initialized_output_df$r__density [cur_result_row]                                       = r__density
+    
+        #  Derived Xu options
+    initialized_output_df$num_nodes_per_group [cur_result_row]                             = num_nodes_per_group
+    initialized_output_df$tot_num_nodes [cur_result_row]                                    = tot_num_nodes
+    initialized_output_df$num_independent_set_nodes [cur_result_row]                        = num_independent_set_nodes
+    initialized_output_df$num_dependent_set_nodes [cur_result_row]                          = num_dependent_set_nodes
+    initialized_output_df$num_rounds_of_linking_between_groups [cur_result_row]            = num_rounds_of_linking_between_groups
+    initialized_output_df$target_num_links_between_2_groups_per_round [cur_result_row]     = target_num_links_between_2_groups_per_round
+    initialized_output_df$num_links_within_one_group [cur_result_row]                      = num_links_within_one_group
+    initialized_output_df$tot_num_links_inside_groups [cur_result_row]                     = tot_num_links_inside_groups
+    initialized_output_df$max_possible_num_links_between_groups [cur_result_row]           = max_possible_num_links_between_groups
+    initialized_output_df$max_possible_tot_num_links [cur_result_row]                       = max_possible_tot_num_links
+    
+        #  Full runset name
+    initialized_output_df$runset_name [cur_result_row]                                      = parameters$runset_name
+    
+        #  Write data frame to file.
+    write_results_to_files (initialized_output_df, parameters, 
+                            cur_result_row)    #  Added 2016 03 28 - BTL.
+    }
+
+#===============================================================================
+
+
+
