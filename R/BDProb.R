e3681dfefe0921c87e401b2c583ea8a187860ded
Bill Langford <bill.langford@rmit.edu.au>
2017-02-09 10:06:16 +1100

    Comments looking at possible overlaps between sdm & res sel data structures
    Seems like there is a lot of possible overlap between species distribution
    modelling and reserve selection data structures, e.g., all of the data about
    planning units.  However, sdm doesn't generally call them planning units and
    they're more often at the pixel level, so the representation is more likely
    to be a map than a list or a data frame.
    
    So, it probably doesn't make sense to share data structures via inheritance,
    but I'll just keep this commit around to save the information.

--- R/BDProb.R
+++ R/BDProb.R
@@ -127,48 +127,123 @@ setClass ("PU_spp_pair_info_class",
 
 #===============================================================================
 
+# Seems like there is a lot of possible overlap between species distribution
+# modelling and reserve selection data structures, e.g., all of the data about
+# planning units.  However, sdm doesn't generally call them planning units and
+# they're more often at the pixel level, so the representation is more likely
+# to be a map than a list or a data frame.
+#
+# So, it probably doesn't make sense to share data structures via inheritance,
+# but I'll just keep this commit around to save the information.
+
+
+    #  Spp_Dist_problem class.        2017 02 29 - BTL - JUST TRYING THIS OUT FOR A MOMENT TO SEE IF THERE IS OVERLAP BETWEEN SD AND RES SEL DATA STRUCTURES TO KEEP IN MIND.
+
+setClass ("Spp_Dist_problem",
+          representation (
+                          UUID                             = "character",                  #  both
+
+                                #  Known for all Xu problems, by definition.
+cor_optimum_cost                 = "numeric",                  #  bd_prob
+                          PU_costs                         = "vector",   #  of numeric   #  both
+
+                          PU_col_name                      = "character",                  #  both
+            spp_col_name                     = "character",                  #  both MAYBE, e.g., if using joint distributions to predict
+                          presences_col_name               = "character",                  #  both
+
+                          correct_solution_vector_is_known = "logical",                  #  both
+                          prob_generator_params_known      = "logical",                  #  both
+
+                          prob_is_ok                       = "logical",                  #  both
+
+read_Xu_problem_from_Xu_file     = "logical",                  #  XU only
+infile_name                      = "character",                  #  Xu only
+
+Xu_parameters                    = "Xu_params",                  #  XU only
+
+num_spp                          = "numeric",                  #  bd_prob
+                          num_PUs                          = "numeric",                  #  both
+            PU_spp_pair_indices              = "data.frame",                  #  both? - but may be different forms
+                          all_PU_IDs                       = "numeric",                  #  both
+            all_spp_IDs                      = "numeric",                  #  both MAYBE, e.g., if using joint distributions to predict
+
+nodes                            = "data.frame",                  #  not sure - XU only or is this just the list of planning units?
+dependent_node_IDs               = "numeric",                  #  XU only?  or, this is named wrong and should be "solution set if known"
+final_link_counts_for_each_node  = "data.frame",                  #  XU only
+
+            bpm                              = "matrix",                  #  both MAYBE, e.g., if using joint distributions to predict
+
+            starting_dir                     = "character",                  #  both?
+            base_outdir                      = "character",                  #  both?
+            derived_bdpg_dir_names           = "list",                  #  both?
+            full_saved_bdprob_path           = "character",                  #  both?
+
+bipartite_metrics_from_bipartite_package = "matrix",                  #  bd_prob
+bipartite_metrics_from_igraph_package_df = "data.frame"                  #  bd_prob
+                          ),
+          prototype (
+                          PU_col_name                      = "PU_ID",
+                          spp_col_name                     = "spp_ID",
+                          presences_col_name               = "freq",
+
+                          bipartite_metrics_from_bipartite_package = NULL,
+                          bipartite_metrics_from_igraph_package_df = NULL
+                    )
+         )
+
+# setMethod ("show", "Xu_bd_problem",
+#            function (object)
+#                {
+#                cat ("Xu_bd_problem = ",
+#                     " \n ")
+#            }
+#           )
+
+#===============================================================================
+
     #  Xu_bd_problem class.
 
 setClass ("Xu_bd_problem",
           representation (
-                          UUID                             = "character",
+                          UUID                             = "character",                  #  bd_prob
 
                                 #  Known for all Xu problems, by definition.
-                          cor_optimum_cost                 = "numeric",
-                          PU_costs                         = "vector",   #  of numeric
+                          cor_optimum_cost                 = "numeric",                  #  bd_prob (only known sometimes) - should this not be COR?, i.e., apparents may have what they perceive to be the opt cost
+                          PU_costs                         = "vector",   #  of numeric   #  bd_prob
 
-                          PU_col_name                      = "character",
-                          spp_col_name                     = "character",
-                          presences_col_name               = "character",
+                          PU_col_name                      = "character",                  #  bd_prob
+                          spp_col_name                     = "character",                  #  bd_prob
+                          presences_col_name               = "character",                  #  bd_prob
 
-                          correct_solution_vector_is_known = "logical",
-                          prob_generator_params_known      = "logical",
-                          prob_is_ok                       = "logical",
+                          correct_solution_vector_is_known = "logical",                  #  bd_prob
+                          prob_generator_params_known      = "logical",                  #  bd_prob
 
-                          read_Xu_problem_from_Xu_file     = "logical",
-                          infile_name                      = "character",
+                          prob_is_ok                       = "logical",                  #  bd_prob
 
-                          Xu_parameters                    = "Xu_params",
+read_Xu_problem_from_Xu_file     = "logical",                  #  XU only
+infile_name                      = "character",                  #  Xu only
 
-                          num_spp                          = "numeric",
-                          num_PUs                          = "numeric",
-                          PU_spp_pair_indices              = "data.frame",
-                          all_PU_IDs                       = "numeric",
-                          all_spp_IDs                      = "numeric",
+Xu_parameters                    = "Xu_params",                  #  XU only
 
-                          nodes                            = "data.frame",
-                dependent_node_IDs               = "numeric",
-                          final_link_counts_for_each_node  = "data.frame",
+                          num_spp                          = "numeric",                  #  bd_prob
+                          num_PUs                          = "numeric",                  #  bd_prob
+PU_spp_pair_indices              = "data.frame",                  #  bd_prob - but may be different forms
+                          all_PU_IDs                       = "numeric",                  #  bd_prob
+                          all_spp_IDs                      = "numeric",                  #  bd_prob
 
-                          bpm                              = "matrix",
+nodes                            = "data.frame",                  #  not sure - XU only or is this just the list of planning units?
+dependent_node_IDs               = "numeric",                  #  XU only?  or, this is named wrong and should be "solution set if known"
+final_link_counts_for_each_node  = "data.frame",                  #  XU only
 
-                          starting_dir                     = "character",
-                          base_outdir                      = "character",
-                          derived_bdpg_dir_names           = "list",
-                          full_saved_bdprob_path           = "character",
+                          bpm                              = "matrix",                  #  bd_prob - but could be integer or float, depending on problem
 
-                          bipartite_metrics_from_bipartite_package = "matrix",
-                          bipartite_metrics_from_igraph_package_df = "data.frame"
+                          starting_dir                     = "character",                  #  bd_prob
+                          base_outdir                      = "character",                  #  bd_prob
+                          derived_bdpg_dir_names           = "list",                  #  bd_prob
+                          full_saved_bdprob_path           = "character",                  #  bd_prob
+
+                          bipartite_metrics_from_bipartite_package = "matrix",                  #  bd_prob
+                          bipartite_metrics_from_igraph_package_df = "data.frame"                  #  bd_prob
                           ),
           prototype (
                           PU_col_name                      = "PU_ID",
@@ -201,9 +276,15 @@ setClass ("Xu_wrapped_bd_problem",
 
 #===============================================================================
 
-    #  Xu_bd_problem_app class.
+    #  Xu_bd_problem_APP class.
 
-setClass ("Xu_bd_problem_app",
+            #***  IS THERE A PROBLEM HERE WITH HAVING AN APP VERSION OF WRAPPED OR COMBINED XU PROBLEM OR EVEN OF NON-XU PROBLEM?
+            #     Do we need an app version of every problem class?
+            #     Or, is APP an attribute of any problem and in COR, it's empty?
+            #         For example, there could be a function:
+            #             is.COR=function(prob) {is.null(prob@APP)}
+
+setClass ("Xu_bd_problem_APP",
           representation (
                             UUID_of_base_problem_that_has_err_added = "character",  #  UUID string
                             ret_vals_from_add_errors = "list"
