8317f298b241990eb2aeb3425376814c68c8efee
Bill Langford <bill.langford@rmit.edu.au>
2017-02-24 17:00:02 +1100

    Add code to compute histograms from marxan puvspr input file

---
+++ R/compute_pu_and_spp_histograms_from_marxan_pusvpr_file.R
@@ -0,0 +1,18 @@
+infile_name <- "/Users/bill/tzar/outputdata/biodivprobgen/default_runset/1834_marxan_simulated_annealing.completedTzarEmulation/RSrun_-COR-Wrap-Marxan_SA.408782d5-2bb6-4c09-8b18-f05ad3089b15/input/puvspr.dat"
+
+pu_spp_amt_df <- read.csv (infile_name, header=TRUE)
+
+
+    #  Currently, these ignore the amount of each species and just
+    #  count each patch occurrence as an abundance of 1.
+    #  Should probably do it both ways.
+
+
+grouped_by_pu <- dplyr::group_by (pu_spp_amt_df, pu)
+num_spp_per_patch <- dplyr::count_ (grouped_by_pu)
+hist (num_spp_per_patch$n)
+
+grouped_by_spp <- dplyr::group_by (pu_spp_amt_df, species)
+num_PUs_per_spp <- dplyr::count_ (grouped_by_spp)
+hist (num_PUs_per_spp$n)
+
