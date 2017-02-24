ff28972661882661e2592241bd2e949e6fe4f9dc
Bill Langford <bill.langford@rmit.edu.au>
2017-02-24 17:56:07 +1100

    Save histograms of marxan puvspr spp and pu data

--- R/compute_pu_and_spp_histograms_from_marxan_pusvpr_file.R
+++ R/compute_pu_and_spp_histograms_from_marxan_pusvpr_file.R
@@ -8,11 +8,17 @@ pu_spp_amt_df <- read.csv (infile_name, header=TRUE)
     #  Should probably do it both ways.
 
 
-grouped_by_pu <- dplyr::group_by (pu_spp_amt_df, pu)
-num_spp_per_patch <- dplyr::count_ (grouped_by_pu)
-hist (num_spp_per_patch$n)
+# grouped_by_pu <- dplyr::group_by (pu_spp_amt_df, pu)
+# #num_spp_per_patch <- dplyr::count_ (grouped_by_pu, pu)
+# num_spp_per_patch <- dplyr::count_ (pu_spp_amt_df, pu)
+# hist (num_spp_per_patch$n)
+#
+# grouped_by_spp <- dplyr::group_by (pu_spp_amt_df, species)
+# num_PUs_per_spp <- dplyr::count_ (grouped_by_spp, species)
+# hist (num_PUs_per_spp$n)
 
-grouped_by_spp <- dplyr::group_by (pu_spp_amt_df, species)
-num_PUs_per_spp <- dplyr::count_ (grouped_by_spp)
-hist (num_PUs_per_spp$n)
+num_spp_per_patch <- dplyr::count (pu_spp_amt_df, pu)
+hist_num_spp_per_patch <- hist (num_spp_per_patch$n)
 
+num_PUs_per_spp <- dplyr::count (pu_spp_amt_df, species)
+hist_num_PUs_per_spp <- hist (num_PUs_per_spp$n)
