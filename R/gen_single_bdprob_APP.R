bd526903650af51050b95d4271384bdb8c9653c3
Bill Langford <bill.langford@rmit.edu.au>
2017-07-19 18:56:46 +1000

    Refactor components of generating apparent problem.

--- R/gen_single_bdprob_APP.R
+++ R/gen_single_bdprob_APP.R
@@ -6,9 +6,148 @@
 
 #-------------------------------------------------------------------------------
 
-#' Generate a single biodiversity problem with error added to it
+#' Create and initialize apparent bdproblem from a correct problem.
 #'
-#' Create an apparent problem from a correct problem.
+#-------------------------------------------------------------------------------
+
+create_and_init_APP_bdprob <- function (Xu_bdprob_COR)
+    {
+        #------------------------------------------------------------
+        #  Save data known so far for the newly created Xu problem.
+        #  Note that the base class here may be a basic Xu problem
+        #  or a wrapped one.
+        #  If it's not a basic one, then extra data may need to be
+        #  copied into the APP problem.
+        #------------------------------------------------------------
+
+#    Xu_bdprob_APP = new ("Xu_bd_problem")
+    Xu_bdprob_APP = new (class (Xu_bdprob_COR))
+
+        #------------------------------------------------------------
+        #  If the COR problem is a wrapped problem,
+        #  then you need to copy the UUID of its base problem.
+        #------------------------------------------------------------
+
+    if (Xu_bdprob_COR@basic_or_wrapped_or_comb_str == "Wrap")
+        Xu_bdprob_APP@UUID_of_base_problem_that_is_wrapped =
+            Xu_bdprob_COR@UUID_of_base_problem_that_is_wrapped
+
+        #---------------------------------------------------------------
+        #  Assign a unique identifier to this newly generated problem.
+        #  These IDs are useful when combining or adding error to
+        #  problems so that you can identify exactly which problems
+        #  were combined or used as a base when provenance might get
+        #  confusing.
+        #---------------------------------------------------------------
+
+    Xu_bdprob_APP@UUID = uuid::UUIDgenerate()
+    Xu_bdprob_APP@prob_is_ok                       = FALSE
+
+        #------------------------------------------------------------------
+        #  Save whatever information is known about the problem generator
+        #  that produced this problem.
+        #------------------------------------------------------------------
+
+    Xu_bdprob_APP@prob_type     = Xu_bdprob_COR@prob_type
+    Xu_bdprob_APP@prob_gen_info = Xu_bdprob_COR@prob_gen_info
+
+    Xu_bdprob_APP@prob_generator_params_known      = Xu_bdprob_COR@prob_generator_params_known
+    Xu_bdprob_APP@correct_solution_vector_is_known = Xu_bdprob_COR@correct_solution_vector_is_known
+
+#CHANGE THESE?
+    Xu_bdprob_APP@PU_spp_pair_indices       = Xu_bdprob_COR@PU_spp_pair_indices
+# DO THE IDS NEED TO BE SAME AS COR THOUGH?
+    Xu_bdprob_APP@all_PU_IDs                = Xu_bdprob_COR@all_PU_IDs
+    Xu_bdprob_APP@all_spp_IDs               = Xu_bdprob_COR@all_spp_IDs
+
+#CHANGE THESE?
+    Xu_bdprob_APP@PU_col_name               = Xu_bdprob_COR@PU_col_name
+    Xu_bdprob_APP@spp_col_name              = Xu_bdprob_COR@spp_col_name
+    Xu_bdprob_APP@num_PUs                   = Xu_bdprob_COR@num_PUs
+    Xu_bdprob_APP@num_spp                   = Xu_bdprob_COR@num_spp
+    Xu_bdprob_APP@correct_solution_cost          = Xu_bdprob_COR@correct_solution_cost
+#IN FUTURE ERROR MODELS, THESE COSTS COULD BE CHANGED.
+    Xu_bdprob_APP@PU_costs                  = Xu_bdprob_COR@PU_costs
+
+#WHAT IS STORED IN NODES?  NOT SURE IF IT NEEDS TO CHANGE OR NOT.
+#I SUSPECT IT'S ONLY USED IN GENERATION OF THE ORIGINAL XU PROBLEM.
+    Xu_bdprob_APP@nodes                     = Xu_bdprob_COR@nodes
+
+    return (Xu_bdprob_APP)
+    }
+
+#===============================================================================
+
+    #-------------------------------------------------
+    #  Create directories for this apparent problem.
+    #-------------------------------------------------
+
+create_dirs_for_APP_prob <- function (Xu_bdprob_APP,
+                                      basic_or_wrapped_or_comb_str,
+                                      starting_dir
+                                      )
+    {
+        #  Build file name prefix.
+    Xu_bdprob_APP@obj_type_str                     = "RSprob"
+    Xu_bdprob_APP@basic_or_wrapped_or_comb_str     = basic_or_wrapped_or_comb_str
+    Xu_bdprob_APP@cor_or_app_str                   = "APP"
+    Xu_bdprob_APP@file_name_prefix =
+                            paste (Xu_bdprob_APP@obj_type_str,
+                                   Xu_bdprob_APP@cor_or_app_str,
+                                   Xu_bdprob_APP@basic_or_wrapped_or_comb_str,
+                                   sep='-')
+
+        #  Create directories.
+    create_RSprob_dir_and_subdirs (starting_dir, Xu_bdprob_APP)
+
+    return (Xu_bdprob_APP)
+    }
+
+#===============================================================================
+
+    #-----------------------------------------------------------------
+    #  Compute and save the distribution and network metrics for the
+    #  problem.
+    #-----------------------------------------------------------------
+
+compute_and_save_dist_and_network_metrics_for_prob <- function (Xu_bdprob_APP,
+                                                                Xu_bdprob_COR,
+                                                                compute_network_metrics_for_this_prob,
+                                                                starting_dir,
+                                                                parameters
+                                                                )
+    {
+        #  Summarize and plot graph and distribution structure information.
+    Xu_bdprob_APP@final_link_counts_for_each_node =
+        summarize_and_plot_graph_and_distribution_structure_information (
+                  Xu_bdprob_APP@PU_spp_pair_indices,
+                  "APP",
+                  Xu_bdprob_COR@all_PU_IDs,    #####!!!!!#####all_correct_node_IDs,
+
+#                  Xu_bdprob_APP@derived_bdpg_dir_names$plot_output_dir,
+                  get_RSprob_path_plots (Xu_bdprob_APP, starting_dir),
+
+                  Xu_bdprob_APP@spp_col_name,
+                  Xu_bdprob_APP@PU_col_name,
+                  Xu_bdprob_APP@presences_col_name
+                  )
+
+        #  Compute network metrics.
+    Xu_bdprob_APP <- init_object_graph_data (Xu_bdprob_APP,
+                                             starting_dir,
+                                             parameters$compute_network_metrics,
+                                #parameters$compute_network_metrics_APP,
+                                compute_network_metrics_for_this_prob,
+                                             parameters$use_igraph_metrics,
+                                             parameters$use_bipartite_metrics,
+                                             parameters$bipartite_metrics_to_use)
+
+    return (Xu_bdprob_APP)
+    }
+
+#===============================================================================
+
+#' Generate a single biodiversity problem with error added to it
 #'
 #-------------------------------------------------------------------------------
 
@@ -97,77 +236,11 @@ gen_single_bdprob_APP = function (Xu_bdprob_COR,
                         app_dir_name_stem = "app"
                                     )
     {
-        #------------------------------------------------------------
-        #  Save data known so far for the newly created Xu problem.
-        #  Note that the base class here may be a basic Xu problem
-        #  or a wrapped one.
-        #  If it's not a basic one, then extra data may need to be
-        #  copied into the APP problem.
-        #------------------------------------------------------------
+    Xu_bdprob_APP = create_and_init_APP_bdprob (Xu_bdprob_COR)
 
-#    Xu_bdprob_APP = new ("Xu_bd_problem")
-    Xu_bdprob_APP = new (class (Xu_bdprob_COR))
-
-        #------------------------------------------------------------
-        #  If the COR problem is a wrapped problem,
-        #  then you need to copy the UUID of its base problem.
-        #------------------------------------------------------------
-
-    if (Xu_bdprob_COR@basic_or_wrapped_or_comb_str == "Wrap")
-        Xu_bdprob_APP@UUID_of_base_problem_that_is_wrapped =
-            Xu_bdprob_COR@UUID_of_base_problem_that_is_wrapped
-
-        #---------------------------------------------------------------
-        #  Assign a unique identifier to this newly generated problem.
-        #  These IDs are useful when combining or adding error to
-        #  problems so that you can identify exactly which problems
-        #  were combined or used as a base when provenance might get
-        #  confusing.
-        #---------------------------------------------------------------
-
-    Xu_bdprob_APP@UUID = uuid::UUIDgenerate()
-    Xu_bdprob_APP@prob_is_ok                       = FALSE
-
-        #------------------------------------------------------------------
-        #  Save whatever information is known about the problem generator
-        #  that produced this problem.
-        #------------------------------------------------------------------
-
-    Xu_bdprob_APP@prob_type     = Xu_bdprob_COR@prob_type
-    Xu_bdprob_APP@prob_gen_info = Xu_bdprob_COR@prob_gen_info
-
-    Xu_bdprob_APP@prob_generator_params_known      = Xu_bdprob_COR@prob_generator_params_known
-    Xu_bdprob_APP@correct_solution_vector_is_known = Xu_bdprob_COR@correct_solution_vector_is_known
-
-#CHANGE THESE?
-    Xu_bdprob_APP@PU_spp_pair_indices       = Xu_bdprob_COR@PU_spp_pair_indices
-# DO THE IDS NEED TO BE SAME AS COR THOUGH?
-    Xu_bdprob_APP@all_PU_IDs                = Xu_bdprob_COR@all_PU_IDs
-    Xu_bdprob_APP@all_spp_IDs               = Xu_bdprob_COR@all_spp_IDs
-
-#CHANGE THESE?
-    Xu_bdprob_APP@PU_col_name               = Xu_bdprob_COR@PU_col_name
-    Xu_bdprob_APP@spp_col_name              = Xu_bdprob_COR@spp_col_name
-    Xu_bdprob_APP@num_PUs                   = Xu_bdprob_COR@num_PUs
-    Xu_bdprob_APP@num_spp                   = Xu_bdprob_COR@num_spp
-    Xu_bdprob_APP@correct_solution_cost          = Xu_bdprob_COR@correct_solution_cost
-#IN FUTURE ERROR MODELS, THESE COSTS COULD BE CHANGED.
-    Xu_bdprob_APP@PU_costs                  = Xu_bdprob_COR@PU_costs
-
-#WHAT IS STORED IN NODES?  NOT SURE IF IT NEEDS TO CHANGE OR NOT.
-#I SUSPECT IT'S ONLY USED IN GENERATION OF THE ORIGINAL XU PROBLEM.
-    Xu_bdprob_APP@nodes                     = Xu_bdprob_COR@nodes
-
-        #-------------------------------------------------------------
-        #  App data.
-        #-------------------------------------------------------------
-
-
-#===============================================================================
-#===============================================================================
-#===============================================================================
-#                   Add error to the species occupancy data.
-#===============================================================================
+        #--------------------------------------------
+        #  Add error to the species occupancy data.
+        #--------------------------------------------
 
     APP_prob_info = new ("APP_prob_info_class")
 
@@ -213,105 +286,75 @@ gen_single_bdprob_APP = function (Xu_bdprob_COR,
         #  Set the values for the apparent problem structure.
     APP_prob_info@app_PU_spp_pair_indices      = ret_vals_from_apply_errors$app_PU_spp_pair_indices
 
+    Xu_bdprob_APP@APP_prob_info          = APP_prob_info
+
 #===============================================================================
 
-    Xu_bdprob_APP@APP_prob_info          = APP_prob_info
+        #--------------------------
+        #  Save adjacency matrix.
+        #--------------------------
 
     #NEEDS TO HAVE SAME DIMENSIONS AND ROW/COLUMN NAMES AS COR.
     Xu_bdprob_APP@bpm                      = ret_vals_from_apply_errors$app_spp_occupancy_data
 
-#browser()
-
-#===============================================================================
-#===============================================================================
 #===============================================================================
 
+            #-------------------------------------------------------
+            #  Old code not needed anymore?
+            #----------------------------------------------------
 
-        #-----------------------------------------------------------
-        #  Convert PU/spp data structure into other formats needed
-        #  downstream.
-        #-----------------------------------------------------------
+                        #----------------------------------------------------
+                        #  Convert PU/spp data structure into other formats
+                        #  needed downstream.
+                        #----------------------------------------------------
 
-    # bpm =
-        # create_adj_matrix_with_spp_rows_vs_PU_cols (Xu_bdprob_APP@num_spp,
-        #                                             Xu_bdprob_APP@num_PUs,
-        #                                             Xu_bdprob_APP@PU_spp_pair_indices,
-        #                                     Xu_bdprob_APP@PU_costs,
-        #                                             Xu_bdprob_APP@spp_col_name,
-        #                                             Xu_bdprob_APP@PU_col_name,
-        #                                             PU_spp_pair_info@dependent_node_IDs,
-        #                                             PU_spp_pair_info@correct_solution_vector_is_known,
-        #                                             bdpg_error_codes)
+                    # bpm =
+                        # create_adj_matrix_with_spp_rows_vs_PU_cols (Xu_bdprob_APP@num_spp,
+                        #                                             Xu_bdprob_APP@num_PUs,
+                        #                                             Xu_bdprob_APP@PU_spp_pair_indices,
+                        #                                     Xu_bdprob_APP@PU_costs,
+                        #                                             Xu_bdprob_APP@spp_col_name,
+                        #                                             Xu_bdprob_APP@PU_col_name,
+                        #                                             PU_spp_pair_info@dependent_node_IDs,
+                        #                                             PU_spp_pair_info@correct_solution_vector_is_known,
+                        #                                             bdpg_error_codes)
 
-#    Xu_bdprob_APP@bpm = app_bpm
-
-
-        #-------------------------------------------------------------
-        #  Quit if there are any duplicate edges/spp in the problem.
-        #-------------------------------------------------------------
-
-# CHANGE THIS?
-#  2017 02 10 - BTL
-#  Getting error when I run this, but I don't think it should be an error
-#  because if you add false positives, then I think that would allow you to get
-#  duplicate links.  So, I'm going to remove this call for apparent, but not
-#  for correct.
-
-#     see_if_there_are_any_duplicate_links (Xu_bdprob_APP@bpm,
-#
-# #                                          Xu_bdprob_APP@num_spp,
-#                                           Xu_bdprob_COR@num_spp,   #  correct?
-#
-#                                           bdpg_error_codes)
+                #    Xu_bdprob_APP@bpm = app_bpm
 
 
-        #----------------------------------------
-        #  Create directories for this problem.
-        #----------------------------------------
+                        #-------------------------------------------------------------
+                        #  Quit if there are any duplicate edges/spp in the problem.
+                        #-------------------------------------------------------------
 
-    Xu_bdprob_APP@obj_type_str                     = "RSprob"
-    Xu_bdprob_APP@basic_or_wrapped_or_comb_str     = Xu_bdprob_COR@basic_or_wrapped_or_comb_str
-    Xu_bdprob_APP@cor_or_app_str                   = "APP"
+                # CHANGE THIS?
+                #  2017 02 10 - BTL
+                #  Getting error when I run this, but I don't think it should be an error
+                #  because if you add false positives, then I think that would allow you to get
+                #  duplicate links.  So, I'm going to remove this call for apparent, but not
+                #  for correct.
 
-    Xu_bdprob_APP@file_name_prefix =
-                            paste (Xu_bdprob_APP@obj_type_str,
-                                   Xu_bdprob_APP@cor_or_app_str,
-                                   Xu_bdprob_APP@basic_or_wrapped_or_comb_str,
-                                   sep='-')
+                #     see_if_there_are_any_duplicate_links (Xu_bdprob_APP@bpm,
+                #
+                # #                                          Xu_bdprob_APP@num_spp,
+                #                                           Xu_bdprob_COR@num_spp,   #  correct?
+                #
+                #                                           bdpg_error_codes)
+
+#===============================================================================
 
     starting_dir = parameters$fullOutputDir_NO_slash
 
-    create_RSprob_dir_and_subdirs (starting_dir, Xu_bdprob_APP)
+    Xu_bdprob_APP =
+        create_dirs_for_APP_prob (Xu_bdprob_APP,
+                                  Xu_bdprob_COR@basic_or_wrapped_or_comb_str,
+                                  starting_dir)
 
-        #-----------------------------------------------------------------
-        #  Compute and save the distribution and network metrics for the
-        #  problem.
-        #-----------------------------------------------------------------
-
-        #  Summarize and plot graph and distribution structure information.
-    Xu_bdprob_APP@final_link_counts_for_each_node =
-        summarize_and_plot_graph_and_distribution_structure_information (
-                  Xu_bdprob_APP@PU_spp_pair_indices,
-                  "APP",
-                  Xu_bdprob_COR@all_PU_IDs,    #####!!!!!#####all_correct_node_IDs,
-
-#                  Xu_bdprob_APP@derived_bdpg_dir_names$plot_output_dir,
-                  get_RSprob_path_plots (Xu_bdprob_APP, starting_dir),
-
-                  Xu_bdprob_APP@spp_col_name,
-                  Xu_bdprob_APP@PU_col_name,
-                  Xu_bdprob_APP@presences_col_name
-                  )
-
-        #  Compute network metrics.
-    Xu_bdprob_APP <- init_object_graph_data (Xu_bdprob_APP,
-                                             starting_dir,
-                                             parameters$compute_network_metrics,
-                                #parameters$compute_network_metrics_APP,
-                                compute_network_metrics_for_this_prob,
-                                             parameters$use_igraph_metrics,
-                                             parameters$use_bipartite_metrics,
-                                             parameters$bipartite_metrics_to_use)
+    Xu_bdprob_APP =
+        compute_and_save_dist_and_network_metrics_for_prob (Xu_bdprob_APP,
+                                                            Xu_bdprob_COR,
+                                                            compute_network_metrics_for_this_prob,
+                                                            starting_dir,
+                                                            parameters)
 
         #------------------------------------------------------------
         #  Everything seems to have worked.
