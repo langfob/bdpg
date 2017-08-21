8aa6b7e327703b60d49f4e25d3ca44edddd4dc30
Bill Langford <bill.langford@rmit.edu.au>
2017-08-21 18:51:25 +1000

    Initialize random seed using new function rather than parameters$seed

--- R/biodivprobgen_initialization.R
+++ R/biodivprobgen_initialization.R
@@ -159,8 +159,9 @@ get_integerize_function <- function (integerize_string)
 init_for_bdpg <- function (parameters)
     {
         #  Set random seed to help reproducibility.
-        #  Has to be done after startup code that loads parameters structure.
-    set.seed (parameters$seed)
+#        #  Has to be done after startup code that loads parameters structure.
+#    set.seed (parameters$seed)
+    get_and_set_new_rand_seed (forced_seed)
 
         #  Initialize error codes.
     bdpg_error_codes        = get_bdpg_error_codes ()
