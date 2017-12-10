877ed3af9e1ec0d6e084df59fbc30e9fe7071b57
Bill Langford <bill.langford@rmit.edu.au>
2017-12-10 14:04:38 +1100

    Add 2 tests to testthat dummy routines to see how they behave

--- tests/testthat/test-placeholder.R
+++ tests/testthat/test-placeholder.R
@@ -4,3 +4,12 @@ test_that("spurious test of equality to make CHECK happy", {
 expect_equal(TRUE,TRUE)
 })
 
+test_that("error happens", {
+    expect_error ("nada")
+})
+
+test_that("error happens", {
+    expect_error (stop("a msg at stop time"))
+})
+
+
