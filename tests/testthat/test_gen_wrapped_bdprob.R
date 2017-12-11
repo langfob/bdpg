49567c7454c3777f4219187a73cdd07537052e5d
Bill Langford <bill.langford@rmit.edu.au>
2017-12-11 12:11:19 +1100

    Moved wrapping tests out of wrap source file into separate test file

---
+++ tests/testthat/test_gen_wrapped_bdprob.R
@@ -0,0 +1,297 @@
+library (bdpg)
+context ("Generating a WRAPPED bdproblem")
+
+#-------------------------------------------------------------------------------
+
+test_create_eligible_PU_set <- function ()
+    {
+    Xu_dep_set = c(1,2)
+    extra_PUs = c(3,4)
+    dep_set_PUs_eligible = TRUE
+
+    eligible_PUs = create_eligible_PU_set (Xu_dep_set, extra_PUs, dep_set_PUs_eligible)
+    if (identical (eligible_PUs, c(1,2,3,4))) cat (".") else cat ("F")
+
+    #----------
+
+    Xu_dep_set = c(1,2)
+    extra_PUs = c(3,4)
+    dep_set_PUs_eligible = FALSE
+
+    eligible_PUs = create_eligible_PU_set (Xu_dep_set, extra_PUs, dep_set_PUs_eligible)
+    if (identical (eligible_PUs, c(3,4))) cat (".") else cat ("F")
+    }
+
+if (FALSE) test_create_eligible_PU_set ()
+
+#-------------------------------------------------------------------------------
+
+test_trim_abundances <- function ()
+    {
+        #  Test endpoints equal to bounds of randomly ordered vector.
+    rounded_abundances = c (8,9,5,3,0,10,7,1,6,2,4)
+    min_abund = 0
+    max_abund = 10
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund,
+                                          max_abund=max_abund)
+    if (identical (trimmed_abundances, rounded_abundances)) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test some elements < min and > max bounds of randomly ordered vector.
+    rounded_abundances = c (8,9,5,3,0,10,7,1,6,2,4)
+    min_abund = 5
+    max_abund = 6
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund,
+                                          max_abund=max_abund)
+    if (identical (trimmed_abundances, c(5,6))) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test elements < min bound only.
+    rounded_abundances = 0:10
+    min_abund = 5
+    max_abund = 10
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund,
+                                          max_abund=max_abund)
+    if (identical (trimmed_abundances, 5:10)) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test elements > max bound only.
+    rounded_abundances = 0:10
+    min_abund = 0
+    max_abund = 8
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund,
+                                          max_abund=max_abund)
+    if (identical (trimmed_abundances, 0:8)) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test all elements < min OR > max bounds of vector.
+    rounded_abundances = c(0,1,2,3,9,10)
+    min_abund = 4
+    max_abund = 8
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund,
+                                          max_abund=max_abund)
+    if (length (trimmed_abundances) == 0) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test elements < min bound only with max bound default argument.
+    rounded_abundances = 0:10
+    min_abund = 3
+
+    trimmed_abundances = trim_abundances (rounded_abundances,
+                                          min_abund=min_abund)
+    if (identical (trimmed_abundances, 3:10)) cat (".") else cat ("F")
+
+    #----------
+
+        #  Test elements < min bound only with min and max bound default args.
+    rounded_abundances = 0:10
+
+    trimmed_abundances = trim_abundances (rounded_abundances)
+    if (identical (trimmed_abundances, 2:10)) cat (".") else cat ("F")
+    }
+
+if (FALSE) test_trim_abundances ()
+
+#-------------------------------------------------------------------------------
+
+test_gen_raw_histogram_of_wrapped_dist <- function ()
+    {
+    #------------------
+    #  Simple example
+    #------------------
+
+        #  Xu_PU_spp_table PU_spp table for original Xu problem being wrapped
+        #  around
+    num_spp = 5
+    num_PU = 10
+    Xu_PU_spp_table = data.frame (PU_ID=1:num_PU, spp_ID=rep(1:num_spp,each=2))
+# Xu_PU_spp_table
+#    PU_ID spp_ID
+# 1      1      1
+# 2      2      1
+# 3      3      2
+# 4      4      2
+# 5      5      3
+# 6      6      3
+# 7      7      4
+# 8      8      4
+# 9      9      5
+# 10    10      5
+
+        #  trimmed_rounded_abund_per_spp vector of abundances of all species in
+        #  the full wrapped distribution, i.e., including the original Xu problem
+        #  abundances
+# trimmed_rounded_abund_per_spp =
+#   [1] 3 2 3 3 2 5 2 2 2 5 2 3 2 6 2
+
+    trimmed_rounded_abund_per_spp = c(3,2,3,3,2,
+                                      5,2,2,2,5,
+                                      2,3,2,6,2)
+
+    spp_col_name = "spp_ID"
+
+    desired_result = data.frame (x = c(2,3,5,6),
+                                 freq.x = c(8, 4, 2, 1),
+                                 freq.y = c(5, NA, NA, NA))
+#   x freq.x freq.y
+# 1 2     8     5
+# 2 3     4     NA
+# 4 5     2     NA
+# 5 6     1     NA
+
+    wrapped_extra_spp_abund_merge =
+        gen_raw_histogram_of_wrapped_dist (Xu_PU_spp_table,
+                                           trimmed_rounded_abund_per_spp,
+                                           spp_col_name)
+    if (all.equal (wrapped_extra_spp_abund_merge, desired_result)) cat (".") else cat ("F")
+#browser()
+
+    #------------------------
+    #  More complex example
+    #------------------------
+
+        #  Xu_PU_spp_table PU_spp table for original Xu problem being wrapped
+        #  around
+    num_spp = 79    # 5
+    num_PU = 158    # 10
+    Xu_PU_spp_table = data.frame (PU_ID=1:num_PU, spp_ID=rep(1:num_spp,each=2))
+# Xu_PU_spp_table
+#    PU_ID spp_ID
+# 1      1      1
+# 2      2      1
+# 3      3      2
+# 4      4      2
+# 5      5      3
+# 6      6      3
+# 7      7      4
+# 8      8      4
+# 9      9      5
+# 10    10      5
+# ...
+# 155   155     78
+# 156   156     78
+# 157   157     79
+# 158   158     79
+
+        #  trimmed_rounded_abund_per_spp vector of abundances of all species in
+        #  the full wrapped distribution, i.e., including the original Xu problem
+        #  abundances
+# trimmed_rounded_abund_per_spp =
+#   [1] 5 2 2 2 2 3 2 3 3 2 2 3 2 6 2 4 2 2 2 2 2 2 2 2 2 2 2 3 3 2 2 3 2 4 2 3 3
+#  [38] 2 3 2 2 2 3 4 2 3 3 2 2 2 2 3 2 2 2 2 3 2 4 2 3 3 2 2 3 4 2 2 2 2 2 3 2 3
+#  [75] 3 3 2 3 3 2 2 3 3 2 3 2 2 3 3 2 2 2 2 3 2 6 2 3 2 4 2 2 3 2 2 3 2 2 3 2 3
+# [112] 4 4 2 2 3 3 2 2 4 2 2 3 3 2 2 3 3 2 3 3 2 2 2 5 2 3 2 2 2 4 2 3 3 2 2
+    trimmed_rounded_abund_per_spp = c(
+        5,2,2,2,2,3,2,3,3,2,2,3,2,6,2,4,2,2,2,2,2,2,2,2,2,2,2,3,3,2,2,3,2,4,2,3,3,
+        2,3,2,2,2,3,4,2,3,3,2,2,2,2,3,2,2,2,2,3,2,4,2,3,3,2,2,3,4,2,2,2,2,2,3,2,3,
+        3,3,2,3,3,2,2,3,3,2,3,2,2,3,3,2,2,2,2,3,2,6,2,3,2,4,2,2,3,2,2,3,2,2,3,2,3,
+        4,4,2,2,3,3,2,2,4,2,2,3,3,2,2,3,3,2,3,3,2,2,2,5,2,3,2,2,2,4,2,3,3,2,2)
+
+    spp_col_name = "spp_ID"
+
+    desired_result = data.frame (x = 2:6,
+                                 freq.x = c(86, 46, 10, 2, 2),
+                                 freq.y = c(79, NA, NA, NA, NA))
+#   x freq.x freq.y
+# 1 2     86     79
+# 2 3     46     NA
+# 3 4     10     NA
+# 4 5      2     NA
+# 5 6      2     NA
+
+    wrapped_extra_spp_abund_merge =
+        gen_raw_histogram_of_wrapped_dist (Xu_PU_spp_table,
+                                           trimmed_rounded_abund_per_spp,
+                                           spp_col_name)
+#browser()
+    if (all.equal (wrapped_extra_spp_abund_merge, desired_result)) cat (".") else cat ("F")
+
+    #--------------------
+
+    }
+
+if (FALSE) test_gen_raw_histogram_of_wrapped_dist ()
+
+#-------------------------------------------------------------------------------
+
+test_compute_final_wrapped_extra_spp_abund_hist <- function ()
+    {
+    #------------------------------------------------------------------
+    #  Test that it fails if the wrapping distribution doesn't
+    #  completely contain the wrapped distribution, e.g.,
+    #  if there are more species on 2 patches in the base Xu problem
+    #  (freq.y) than in the wrapping lognormal distribution (freq.x).
+    #------------------------------------------------------------------
+
+    wrapped_extra_spp_abund_merge = data.frame (x = 2:5,
+                                               freq.x = c(10, 46, 10, 2),
+                                               freq.y = c(20, NA, NA, NA))
+#   x freq.x freq.y
+# 1 2     10     20  <<<<<-----  20 spp on 2 patches in Xu problem but only 10 spp on 2 patches in wrapping distribution
+# 2 3     46     NA
+# 3 4     10     NA
+# 4 5      2     NA
+
+        #  This call should fail, so need to catch the error.
+        #  Could this be done with a call to expect_error() instead?
+    error_in_compute =
+        tryCatch ({ compute_final_wrapped_extra_spp_abund_hist (wrapped_extra_spp_abund_merge)
+                    FALSE
+                  },
+                  error = function (err) { TRUE }
+                 )
+
+    if (error_in_compute) cat (".") else cat ("F")
+
+    #-----------------------
+    #  Test normal example
+    #-----------------------
+
+
+    wrapped_extra_spp_abund_merge = data.frame (x = 2:6,
+                                                freq.x = c(86, 46, 10, 2, 2),
+                                                freq.y = c(79, NA, NA, NA, NA))
+
+    final_wrapped_extra_spp_abund_merge =
+        compute_final_wrapped_extra_spp_abund_hist (wrapped_extra_spp_abund_merge)
+
+    desired_result = data.frame (abund = 2:6,
+                                 freq = c(7, 46, 10, 2, 2))
+
+    if (all.equal (final_wrapped_extra_spp_abund_merge, desired_result)) cat (".") else cat ("F")
+    }
+
+if (FALSE) test_compute_final_wrapped_extra_spp_abund_hist ()
+
+test_that("str_length is number of characters", {
+  expect_equal(str_length("a"), 1)
+  expect_equal(str_length("ab"), 2)
+  expect_equal(str_length("abc"), 3)
+})
+
+test_that("str_length of factor is length of level", {
+  expect_equal(str_length(factor("a")), 1)
+  expect_equal(str_length(factor("ab")), 2)
+  expect_equal(str_length(factor("abc")), 3)
+})
+
+test_that("str_length of missing is missing", {
+  expect_equal(str_length(NA), NA_integer_)
+  expect_equal(str_length(c(NA, 1)), c(NA, 1))
+  expect_equal(str_length("NA"), 2)
+})
