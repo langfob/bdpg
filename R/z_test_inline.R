ff54a5884adaeea7df18cc4d613034b9203ca95e
Bill Langford <bill.langford@rmit.edu.au>
2018-01-24 15:07:45 +1100

    Change which test is run for z_using_inline

--- R/z_test_inline.R
+++ R/z_test_inline.R
@@ -13,12 +13,12 @@ source ("/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpg/R/z_using_inline.R")
 
 verbose = FALSE
 
-zmeths = "inline"
+zmeths = list ("inline")
 
 #zu =test_z (zmeths, seed = 456, num_spp = 4, num_PUs = 3)
-#zu =test_z (zmeths, seed = 456, num_spp = 20, num_PUs = 15)
+zu =test_z (zmeths, seed = 456, num_spp = 20, num_PUs = 15)
 #zu =test_z (zmeths, seed = 456, num_spp = 40, num_PUs = 30)
-zu =test_z (zmeths, seed = 456, num_spp = 400, num_PUs = 300)
+#zu =test_z (zmeths, seed = 456, num_spp = 400, num_PUs = 300)
 
 #zu =test_z (zmeths, seed = 1223, num_spp = 6, num_PUs = 5)
 
