bbed99359bad3fcfaaa54de99e42085ec6922a53
Bill Langford <bill.langford@rmit.edu.au>
2018-01-24 14:07:55 +1100

    Tidying

--- R/z_test_inline.R
+++ R/z_test_inline.R
@@ -12,14 +12,13 @@ source ("/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpg/R/z_test.R")
 source ("/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpg/R/z_using_inline.R")
 
 verbose = FALSE
-brank   = 10
 
 zmeths = "inline"
 
 #zu =test_z (zmeths, seed = 456, num_spp = 4, num_PUs = 3)
-zu =test_z (zmeths, seed = 456, num_spp = 20, num_PUs = 15)
+#zu =test_z (zmeths, seed = 456, num_spp = 20, num_PUs = 15)
 #zu =test_z (zmeths, seed = 456, num_spp = 40, num_PUs = 30)
-#zu =test_z (zmeths, seed = 456, num_spp = 400, num_PUs = 300)
+zu =test_z (zmeths, seed = 456, num_spp = 400, num_PUs = 300)
 
 #zu =test_z (zmeths, seed = 1223, num_spp = 6, num_PUs = 5)
 
