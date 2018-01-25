8a2f7f306b544dfb782092fa9a9c47aef8520cb2
Bill Langford <bill.langford@rmit.edu.au>
2018-01-25 17:02:33 +1100

    dd option to go forward or backward in unprotected richness

--- R/do_unprotected_richness_analysis_and_output.R
+++ R/do_unprotected_richness_analysis_and_output.R
@@ -1,10 +1,10 @@
 #===============================================================================
 
-                #  do_unprotected_richnesss_analysis_and_output.R
+                #  do_unprotected_richness_analysis_and_output.R
 
 #===============================================================================
 
-init_for_choosing_PUs_unprotected_richnesss <- function (input_vars_list)
+init_for_choosing_PUs_unprotected_richness <- function (input_vars_list)
     {
     return (list (bpm = input_vars_list$bpm,
                   spp_rep_targets = input_vars_list$spp_rep_targets))
@@ -12,10 +12,10 @@ init_for_choosing_PUs_unprotected_richnesss <- function (input_vars_list)
 
 #===============================================================================
 
-choose_next_PU_unprotected_richnesss <- function (S_remaining_PUs_vec, vars_list)
+choose_next_PU_unprotected_richness <- function (S_remaining_PUs_vec, vars_list,
+                                                  forward)
     {
     spp_rep_targets = vars_list$spp_rep_targets
-
     bpm             = vars_list$bpm
     num_spp         = dim(bpm)[1]
     num_PUs         = dim(bpm)[2]
@@ -28,7 +28,7 @@ choose_next_PU_unprotected_richnesss <- function (S_remaining_PUs_vec, vars_list
     cur_solution_PUs = all_PUs [-S_remaining_PUs_vec]
 
         #-----------------------------------------------------------------
-        #  Compute the total number of occurrences of each species in
+        #  Compute the total number of occurrences of all species in
         #  the current solution.
         #  If there is only one PU column in the current solution,
         #  then R will complain that it's not a matrix when it tries
@@ -41,48 +41,75 @@ choose_next_PU_unprotected_richnesss <- function (S_remaining_PUs_vec, vars_list
 
     cur_spp_reps_in_solution = rowSums (bpm [, cur_solution_PUs, drop=FALSE])
 
-        #-------------------------------------------------
-        #  Ignore species already meeting their targets.
-        #-------------------------------------------------
+        #------------------------------------------------------------------
+        #  Determine which species already meet their targets so that you
+        #  can remove them from the matrix calculations.
+        #------------------------------------------------------------------
 
+if (forward)
+{
     cur_spp_meeting_or_exceeding_tgt =
             which (cur_spp_reps_in_solution >= spp_rep_targets)
-
+} else
+{
+    cur_spp_meeting_or_exceeding_tgt =
+            which (cur_spp_reps_in_solution >= spp_rep_targets)
+}
     num_spp_meeting_or_exceeding_tgt = length (cur_spp_meeting_or_exceeding_tgt)
-    # if (num_spp_meeting_or_exceeding_tgt < num_spp)
-    #     unprotected_spp_bpm = bpm [-cur_spp_meeting_or_exceeding_tgt,,drop=FALSE]
 
-    if (num_spp_meeting_or_exceeding_tgt < num_spp)
+    if (num_spp_meeting_or_exceeding_tgt == num_spp)
         {
+            #------------------------------------------------------------------
+            #  All species already meet their targets, so the remaining PUs
+            #  are all superflous and it doesn't matter which one you choose.
+            #------------------------------------------------------------------
 
-        unprotected_spp_bpm = bpm [-cur_spp_meeting_or_exceeding_tgt,,drop=FALSE]
-
-
-
-        unprotected_richness_vec_PU = colSums (unprotected_spp_bpm)
-
-        chosen_PUs_vec =
-            which (unprotected_richness_vec_PU ==
-                   max (unprotected_richness_vec_PU[S_remaining_PUs_vec]))
-
-            #  Now we know what are ALL of the PUs in the whole system that
-            #  match the min in S, but some of those can be ones that we've
-            #  already added to the solution set earlier and this can lead
-            #  to the same PU being added to the solution more than once.
-            #  So, now we need to intersect the chosen_PUs_vec with S because
-            #  ONLY PUs in S are allowed to be selected in this round.
-
-        chosen_PUs_vec = chosen_PUs_vec [chosen_PUs_vec %in% S_remaining_PUs_vec]
-        if (length (chosen_PUs_vec) < 1)
-            stop_bdpg ("chosen_PUs_vec is empty in choose_next_PU_unprotected_richnesss")
+        chosen_PUs_vec = S_remaining_PUs_vec
 
         } else
         {
-        chosen_PUs_vec = S_remaining_PUs_vec
+            #----------------------------------------------------------------
+            #  At least one species does not meet its target.
+            #  Strip the already protected species out of the matrix and
+            #  compute the richness of just the unprotected species on each
+            #  PU.
+            #  Again, we have to use drop=FALSE to keep R from converting
+            #  the bpm matrix to a vector when it gets down to 1 row.
+            #----------------------------------------------------------------
+
+        unprotected_spp_bpm = bpm [-cur_spp_meeting_or_exceeding_tgt,, drop=FALSE]
+        unprotected_richness_vec_PU = colSums (unprotected_spp_bpm)
+if (forward)
+{
+            chosen_PUs_vec =
+                which (unprotected_richness_vec_PU ==
+                       max (unprotected_richness_vec_PU[S_remaining_PUs_vec]))
+} else
+{
+            chosen_PUs_vec =
+                which (unprotected_richness_vec_PU ==
+                       min (unprotected_richness_vec_PU[S_remaining_PUs_vec]))
+}
+
+
+            #----------------------------------------------------------------
+            #  Now we know what are ALL of the PUs in the whole system that
+            #  match the min in S, but some of those can be ones that we've
+            #  already added to the solution set earlier.
+            #  To avoid adding the same PU to the solution more than once,
+            #  we need to intersect the chosen_PUs_vec with S because
+            #  ONLY PUs in S are allowed to be selected in this round.
+            #----------------------------------------------------------------
+
+        chosen_PUs_vec = chosen_PUs_vec [chosen_PUs_vec %in% S_remaining_PUs_vec]
+        if (length (chosen_PUs_vec) < 1)
+            stop_bdpg ("chosen_PUs_vec is empty in choose_next_PU_unprotected_richness")
         }
 
-        #  When there is a tie in min of max loss,
-        #  break the tie randomly.
+        #-------------------------------------------------------------------
+        #  When there is a tie in min of max loss, break the tie randomly.
+        #-------------------------------------------------------------------
+
     if (length (chosen_PUs_vec) > 1)
         {
         chosen_PU = break_tie_randomly (chosen_PUs_vec)
@@ -96,7 +123,8 @@ choose_next_PU_unprotected_richnesss <- function (S_remaining_PUs_vec, vars_list
 
 #===============================================================================
 
-unprotected_richnesss_using_funcs <- function (num_spp, num_PUs, bpm,
+unprotected_richness_using_funcs <- function (num_spp, num_PUs, bpm,
+                                               forward,
                                                spp_rep_targets)
     {
     input_vars_list = list (bpm = bpm,
@@ -104,20 +132,22 @@ unprotected_richnesss_using_funcs <- function (num_spp, num_PUs, bpm,
 
     ranked_solution_PU_IDs_vec =
         greedy_using_funcs (num_spp, num_PUs, input_vars_list,
-                            init_for_choosing_PUs_unprotected_richnesss,
-                            choose_next_PU_unprotected_richnesss,
-                            reverse_solution_order = FALSE)
+                            init_for_choosing_PUs_unprotected_richness,
+                            choose_next_PU_unprotected_richness,
+                            forward)
 
     return (ranked_solution_PU_IDs_vec)
     }
 
 #===============================================================================
 
-unprotected_richnesss <- function (num_spp, num_PUs, bpm,
-                                   spp_rep_targets = rep (1, num_spp))
+unprotected_richness <- function (num_spp, num_PUs, bpm,
+                                  forward = TRUE,
+                                  spp_rep_targets = rep (1, num_spp))
     {
     ranked_solution_PU_IDs_vec =
-        unprotected_richnesss_using_funcs (num_spp, num_PUs, bpm,
+        unprotected_richness_using_funcs (num_spp, num_PUs, bpm,
+                                           forward,
                                            spp_rep_targets)
 
     short_ranked_solution_PU_IDs_vec =
@@ -128,44 +158,6 @@ unprotected_richnesss <- function (num_spp, num_PUs, bpm,
     return (list (short_ranked_solution_PU_IDs_vec =
                       short_ranked_solution_PU_IDs_vec,
                   full_ranked_solution_PU_IDs_vec = ranked_solution_PU_IDs_vec))
-    }
-
-#===============================================================================
-
-test_unprotected_richness <- function (seed = 456, num_spp = 4, num_PUs = 3)
-    {
-                                                                                if (verbose) {
-                                                                                cat ("\n  num_spp = ", num_spp)
-                                                                                cat ("\n  num_PUs = ", num_PUs)
-                                                                                }
-    bpm = gen_dummy_bpm (num_spp, num_PUs, seed)
-                                                                                if (verbose) {
-                                                                                cat ("\n\n  bpm = \n")
-                                                                                print (bpm)
-                                                                                }
-
-    set.seed (seed + 12345)
-    timings_using_funcs = system.time ({
-        uruf = unprotected_richnesss (num_spp, num_PUs, bpm)
-    })
-
-    cat ("\n\ntimings_using_funcs = \n")
-    print (timings_using_funcs)
-
-    full_uruf = uruf$full_ranked_solution_PU_IDs_vec
-
-    cat ("\nlength(full_uruf) = ", length (full_uruf))
-    print (full_uruf)
-
-    short_uruf = uruf$short_ranked_solution_PU_IDs_vec
-    cat ("\nlength(short_uruf) = ", length (short_uruf))
-    cat ("\nshort_uruf = \n")
-    print (short_uruf)
-
-    cat ("\n------------------------------------------------------------------------")
-
-    return (list (uruf_long  = uruf$full_ranked_solution_PU_IDs_vec,
-                  uruf_short = uruf$short_ranked_solution_PU_IDs_vec))
     }
 
 #===============================================================================
--- R/test_unprotected_richness.R
+++ R/test_unprotected_richness.R
@@ -4,6 +4,68 @@
 
 #===============================================================================
 
+test_unprotected_richness <- function (seed = 456, num_spp = 4, num_PUs = 3)
+    {
+                                                                                if (verbose) {
+                                                                                cat ("\n  num_spp = ", num_spp)
+                                                                                cat ("\n  num_PUs = ", num_PUs)
+                                                                                }
+    bpm = gen_dummy_bpm (num_spp, num_PUs, seed)
+                                                                                if (verbose) {
+                                                                                cat ("\n\n  bpm = \n")
+                                                                                print (bpm)
+                                                                                }
+
+    forward = TRUE
+    set.seed (seed + 12345)
+    timings_using_funcs_forward = system.time ({
+        uruf_FORWARD = unprotected_richness (num_spp, num_PUs, bpm, forward)
+    })
+
+    cat ("\n\ntimings_using_funcs FORWARD = \n")
+    print (timings_using_funcs_forward)
+
+    full_uruf_FORWARD = uruf_FORWARD$full_ranked_solution_PU_IDs_vec
+
+    cat ("\nlength(full_uruf_FORWARD) = ", length (full_uruf_FORWARD))
+    print (full_uruf_FORWARD)
+
+    short_uruf_FORWARD = uruf_FORWARD$short_ranked_solution_PU_IDs_vec
+    cat ("\nlength(short_uruf_FORWARD) = ", length (short_uruf_FORWARD))
+    cat ("\nshort_uruf_FORWARD = \n")
+    print (short_uruf_FORWARD)
+
+    cat ("\n------------------------------------------------------------------------")
+
+    forward = FALSE
+    set.seed (seed + 12345)
+    timings_using_funcs_BACKWARD = system.time ({
+        uruf_BACKWARD = unprotected_richness (num_spp, num_PUs, bpm, forward)
+    })
+
+    cat ("\n\ntimings_using_funcs_BACKWARD = \n")
+    print (timings_using_funcs_BACKWARD)
+
+    full_uruf_BACKWARD = uruf_BACKWARD$full_ranked_solution_PU_IDs_vec
+
+    cat ("\nlength(full_uruf_BACKWARD) = ", length (full_uruf_BACKWARD))
+    print (full_uruf_BACKWARD)
+
+    short_uruf_BACKWARD = uruf_BACKWARD$short_ranked_solution_PU_IDs_vec
+    cat ("\nlength(short_uruf_BACKWARD) = ", length (short_uruf_BACKWARD))
+    cat ("\nshort_uruf_BACKWARD = \n")
+    print (short_uruf_BACKWARD)
+
+    cat ("\n------------------------------------------------------------------------")
+
+    return (list (uruf_FORWARD_long  = uruf_FORWARD$full_ranked_solution_PU_IDs_vec,
+                  uruf_FORWARD_short = uruf_FORWARD$short_ranked_solution_PU_IDs_vec,
+                  uruf_BACKWARD_long  = uruf_BACKWARD$full_ranked_solution_PU_IDs_vec,
+                  uruf_BACKWARD_short = uruf_BACKWARD$short_ranked_solution_PU_IDs_vec))
+    }
+
+#===============================================================================
+
 options(warn=2)
 library (bdpg)
 
