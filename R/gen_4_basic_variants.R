7fbeb79b37b3dc3a619cd28370cd98696351280c
Bill Langford <bill.langford@rmit.edu.au>
2018-02-11 10:52:22 +1100

    Indent cat() calls to make main actions easier to see

--- R/gen_4_basic_variants.R
+++ R/gen_4_basic_variants.R
@@ -23,7 +23,7 @@
 
 gen_4_basic_variants <- function (parameters, integerize)
     {
-    cat ("\n\nAT START OF gen_4_basic_variants().\n\n")
+                    cat ("\n\nAT START OF gen_4_basic_variants().\n\n")
 
     #===============================================================================
     #       Generate a base problem, i.e, create the Xu graph nodes and edge_list.
@@ -35,14 +35,14 @@ gen_4_basic_variants <- function (parameters, integerize)
                                               cor_dir_name_stem = "cor"
                                               )
 
-    cat ("\n\n-----  base_COR_bd_prob@UUID = '", base_COR_bd_prob@UUID,
-         "', checksum = '", base_COR_bd_prob@checksum, "'  -----\n\n")
+                    cat ("\n\n-----  base_COR_bd_prob@UUID = '", base_COR_bd_prob@UUID,
+                         "', checksum = '", base_COR_bd_prob@checksum, "'  -----\n\n")
 
     do_COR_rs_analysis_and_output (base_COR_bd_prob, parameters)
 
-    cat("\n\njust after set_up_for_and_run_marxan() for Base COR problem")
-    cat ("\n\n================================================================================")
-    cat ("\n================================================================================\n\n")
+                    cat("\n\njust after set_up_for_and_run_marxan() for Base COR problem")
+                    cat ("\n\n================================================================================")
+                    cat ("\n================================================================================\n\n")
 
     #===============================================================================
     #  Generate an APPARENT problem from the base problem, i.e., apply errors.
@@ -56,9 +56,9 @@ gen_4_basic_variants <- function (parameters, integerize)
                                    base_COR_bd_prob,
                                    parameters)
 
-    cat("\n\njust after set_up_for_and_run_marxan() for Base APP problem")
-    cat ("\n\n================================================================================")
-    cat ("\n================================================================================\n\n")
+                    cat("\n\njust after set_up_for_and_run_marxan() for Base APP problem")
+                    cat ("\n\n================================================================================")
+                    cat ("\n================================================================================\n\n")
 
     #===============================================================================
     #       Generate a wrapped problem around the base problem.
@@ -74,9 +74,9 @@ gen_4_basic_variants <- function (parameters, integerize)
 
         do_COR_rs_analysis_and_output (wrapped_COR_bd_prob, parameters)
 
-        cat("\n\njust after set_up_for_and_run_marxan() for Wrapped COR problem")
-        cat ("\n\n================================================================================")
-        cat ("\n================================================================================\n\n")
+                        cat("\n\njust after set_up_for_and_run_marxan() for Wrapped COR problem")
+                        cat ("\n\n================================================================================")
+                        cat ("\n================================================================================\n\n")
 
     #===============================================================================
     #  Generate an APPARENT problem from the wrapped problem, i.e., apply errors.
@@ -90,9 +90,9 @@ gen_4_basic_variants <- function (parameters, integerize)
                                        wrapped_COR_bd_prob,
                                        parameters)
 
-        cat("\n\njust after set_up_for_and_run_marxan() for Wrapped APP problem")
-        cat ("\n\n================================================================================")
-        cat ("\n================================================================================\n\n")
+                        cat("\n\njust after set_up_for_and_run_marxan() for Wrapped APP problem")
+                        cat ("\n\n================================================================================")
+                        cat ("\n================================================================================\n\n")
         }  #  end if - wrap_lognormal_dist_around_Xu
 
     }  #  end function - gen_4_basic_variants()
