f370c5e5cc1098eed3c1d918b7ab39e7dfb4e842
Bill Langford <bill.langford@rmit.edu.au>
2018-10-24 12:31:36 +1100

    Remove "if(FALSE)" statements commenting out do_rs_analysis...() calls

--- R/load_existing_tzar_run.R
+++ R/load_existing_tzar_run.R
@@ -482,13 +482,11 @@ cat ("\nrds_file_path = '", rds_file_path, "'\n")
             cor_bdprob = load_saved_obj_from_file (normalizePath (rds_file_path))
 
             cat ("\nWould now run RS on COR problem '", rds_file_path, "'\n")
-            if (FALSE)
-            {
+
             do_rs_analysis_and_output (cor_bdprob,
                                        cor_bdprob,
                                        parameters,
                                        cur_prob_dir)
-            }
             }  #  end for - COR dirs
         }  #  end if - run rs on COR dirs
 
@@ -524,15 +522,10 @@ cat ("\nrds_file_path = '", rds_file_path, "'\n")
                                                 app_bdprob,
                                                 parameters)
 
-            cat ("\nWould now run RS on APP problem '", rds_file_path, "'\n")
-            if (FALSE)
-            {
             do_rs_analysis_and_output (app_bdprob,
                                        cor_bdprob,
                                        parameters,
                                        cur_prob_dir)
-            }
-
             }  #  end for - APP dirs
         }  #  end if - run rs on APP dirs
 
