f66752d3d0976655a24a2897da55b5aa90ea6d42
Bill Langford <bill.langford@rmit.edu.au>
2018-12-12 16:57:13 +1100

    Move more score-related computations from gscp_15b to gscp_14b
    There was only one function in 15b and it was used in 14b, so I'm just
    moving the function in with the other scoring computations in 14b and
    getting rid of 15b altogether.

--- R/gscp_14b_compute_solution_scores.R
+++ R/gscp_14b_compute_solution_scores.R
@@ -11,6 +11,45 @@
 #  2017 02 18 - BTL - Renamed to gscp_14b_compute_solution_scores.R.
 
 #===============================================================================
+#===============================================================================
+#  2018 12 12 - BTL -
+#  This section moved here from gscp_15b_compute_and_verify_scores.R.
+#-------------------------------------------------------------------------------
+
+# compute_and_verify_APP_rep_scores_according_to_RS <-
+#     function (rs_solution, num_spp, bpm, spp_rep_targets)
+#     {
+#     app_solution_NUM_spp_covered__fromRS =
+#         compute_num_spp_covered_by_solution (rs_solution,
+#                                              bpm,
+#                                              spp_rep_targets)
+#
+#     app_solution_FRAC_spp_covered__fromRS = app_solution_NUM_spp_covered__fromRS / num_spp
+#     app_spp_rep_shortfall__fromRS = 1 - app_solution_FRAC_spp_covered__fromRS
+#
+#     return (list (rsr_app_spp_rep_shortfall__fromRS         = app_spp_rep_shortfall__fromRS,
+#                   rsr_app_solution_NUM_spp_covered__fromRS  = app_solution_NUM_spp_covered__fromRS,
+#                   rsr_app_solution_FRAC_spp_covered__fromRS = app_solution_FRAC_spp_covered__fromRS))
+#     }
+
+#-------------------------------------------------------------------------------
+
+compute_num_spp_covered_by_solution <- function (rs_solution,
+                                                 bpm,
+                                                 spp_rep_targets)
+    {
+        #  Reduce the spp/PU adjacency matrix to only include PUs that
+        #  are in the set to test (e.g., in the proposed reserve set).
+        #  Once that's done, summing each spp row will tell you how much
+        #  representation each spp achieves in the proposed solution.
+
+    spp_reps_in_sol = rowSums (bpm [, rs_solution, drop=FALSE])
+    num_spp_covered = length (which (spp_reps_in_sol >= spp_rep_targets))
+
+    return (num_spp_covered)
+    }
+
+#===============================================================================
 
 #-------------------------------------------------------------------------------
 
@@ -100,7 +139,7 @@ compute_euc_out_err_frac <- function (cor_or_app_str,
     return (results_list)
     }
 
-#===============================================================================
+#-------------------------------------------------------------------------------
 
 compute_RS_solution_cost_scores_wrt_COR_costs_vec <-
                                             function (rs_solution_PU_IDs_vec,
