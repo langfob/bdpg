63619189c68a88ff3d598b34631032826929be1d
Bill Langford <bill.langford@rmit.edu.au>
2018-12-16 14:21:28 +1100

    Add first cut at ensemble RS control logic
    Added code that recognizes the command to run an ensemble and creates the
    RSrun for the ensemble.  It then generates N apparent problems inside the
    RSrun's directory to make up the ensemble.  It also runs marxan on each of
    those apparent runs.  It also adds logic to turn off any other reserve
    selectors when running on these subproblems inside the ensemble.  It still
    runs whichever reserve selectors are selected on the problems at the top
    level, i.e., problems not generated just to make up the ensemble.
    At this point though, it doesn't actually do any aggregation to create
    the ensemble solution.
    solution vectors

--- R/do_rs_analysis_and_output.R
+++ R/do_rs_analysis_and_output.R
@@ -8,6 +8,109 @@
 #
 #===============================================================================
 
+ensemble <- function (APP_bd_prob,
+                      parameters,
+                      ens_probs_starting_dir,
+                      RS_specific_params)
+    {
+    cat ("\nInside ensemble function.\n")
+
+        #  Modify parameters to block graph calculations and to set
+        #  set reserve selectors to block infinite recursion (i.e, don't
+        #  call ensemble again) and to only call reserve selectors that
+        #  are meant to be used to build the ensemble.
+    parameters$compute_network_metrics          = FALSE
+
+    parameters$run_marxan                       = TRUE
+
+    parameters$do_ensemble                      = FALSE
+
+    parameters$do_gurobi                        = FALSE
+    parameters$do_simple_richness_forward       = FALSE
+    parameters$do_simple_richness_backward      = FALSE
+    parameters$do_unprotected_richness_forward  = FALSE
+    parameters$do_unprotected_richness_backward = FALSE
+    parameters$do_zonation_like_forward         = FALSE
+    parameters$do_zonation_like_backward        = FALSE
+
+
+#  2018 12 16 - BTL - For quick testing at the moment...
+FP_err_amt = 0.001
+FN_err_amt = 0.01
+
+#  2018 12 16 - BTL - No cost errors for the moment...
+gen_combined_cost_and_FP_FN_errors = FALSE
+cost_err_amt = 0
+
+    for (cur_prob_idx in 1:RS_specific_params$num_probs_in_ensemble)
+        {
+                #-------------------------------------------
+                #  Variant 3:  FP & FN, counts NOT matched
+                #-------------------------------------------
+
+        gen_1_app_variant (APP_bd_prob,    #base_bd_prob,
+                           parameters,
+                           ens_probs_starting_dir,    #starting_dir,
+
+                           gen_cost_errors = gen_combined_cost_and_FP_FN_errors,
+                           cost_error_frac_bound = cost_err_amt,    #err_amt,
+
+                           gen_FP_FN_errors = TRUE,
+
+                           spp_occ_FP_const_rate = FP_err_amt,    #err_amt,
+                           spp_occ_FN_const_rate = FN_err_amt,    #err_amt,
+
+                           match_error_counts = FALSE)
+        }
+    }
+
+#-------------------------------------------------------------------------------
+
+do_ensemble <- function (APP_bd_prob,    #  <<<<<-----------------
+                         COR_bd_prob,    #  <<<<<-----------------
+                         parameters,
+                         starting_dir,
+
+                         rs_method_name,
+                         resSel_func,
+
+                         RS_specific_params,
+
+                         src_rds_file_dir,
+                         spp_rep_targets)
+    {
+    cat ("\nInside do_ensemble():", sep='')
+    cat ("\n        starting_dir = '", starting_dir, "'", sep='')
+    cat ("\n        rs_method_name = '", rs_method_name, "'", sep='')
+    cat ("\n        RS_specific_params = \n", sep='')
+    print (RS_specific_params)
+    cat ("\n        src_rds_file_dir = '", src_rds_file_dir, "'", sep='')
+    cat ("\n        spp_rep_targets = \n", sep='')
+    print (spp_rep_targets)
+    cat ("\n")
+
+    ResSel_run <- create_RSrun (APP_bd_prob@UUID,
+                                spp_rep_targets,
+                                parameters,
+                                starting_dir,
+                                APP_bd_prob@cor_or_app_str,
+                                APP_bd_prob@basic_or_wrapped_or_comb_str,
+                                rs_method_name)
+
+    base_outdir = get_RSrun_path_topdir (ResSel_run, starting_dir)
+
+    ens_probs_starting_dir = file.path (base_outdir, "Ens_probs")
+    dir.create (ens_probs_starting_dir)
+    cat ("\n\nens_probs_starting_dir = '", ens_probs_starting_dir, "'\n")
+
+    ensemble (APP_bd_prob,
+              parameters,
+              ens_probs_starting_dir,
+              RS_specific_params)
+    }
+
+#===============================================================================
+
 #' Run reserve selector(s) on bdproblem and write output from all analyses
 #'
 #' Note that the COR and APP arguments are the same if running on a correct
@@ -30,6 +133,32 @@ do_rs_analysis_and_output <- function (APP_bd_prob,
                                            spp_rep_targets =
                                                rep(1,COR_bd_prob@num_spp))
     {
+    do_ensemble = vb (parameters$do_ensemble,
+                      def_on_empty = TRUE, def = FALSE)
+
+    if (do_ensemble)
+        {
+        num_probs_in_ensemble = vn (parameters$num_probs_in_ensemble,
+                                    range_lo=2)
+
+        RS_specific_params = list (ensemble_sub_rs_list = c ("marxan"),
+                                   num_probs_in_ensemble = num_probs_in_ensemble)
+        rs_method_name = "Ensemble"
+
+        do_ensemble (APP_bd_prob,    #  <<<<<-----------------
+                     COR_bd_prob,    #  <<<<<-----------------
+                     parameters,
+                     starting_dir,
+
+                     rs_method_name,
+                     resSel_func     = ensemble,
+
+                     RS_specific_params,
+
+                     src_rds_file_dir,
+                     spp_rep_targets)
+        }
+
     do_simple_richness_forward = vb (parameters$do_simple_richness_forward,
                                      def_on_empty = TRUE, def = FALSE)
 
