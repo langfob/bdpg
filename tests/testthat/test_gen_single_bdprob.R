e164dc7b06831ebc54ada57a79fa9773b5bae6ae
Bill Langford <bill.langford@rmit.edu.au>
2018-12-31 13:34:31 +1100

    Add and abstract more tests for gen_single_bdprob.R

--- tests/testthat/test_gen_single_bdprob.R
+++ tests/testthat/test_gen_single_bdprob.R
@@ -40,7 +40,7 @@ test_compute_various_link_cts <- function ()
 
 #===============================================================================
 
-init_test_parameters <- function ()
+init_test_parameters_1 <- function ()
     {
     parameters  = list (bdpg_run_init_rand_seed                      = 123,
 
@@ -104,6 +104,8 @@ test_create_Xu_problem_from_scratch_given_params <- function (parameters)
             parameters$target_num_links_between_2_groups_per_round,
             parameters$num_rounds_of_linking_between_groups,
             parameters$duplicate_links_allowed)
+
+#print (PU_spp_pair_info)
 
     return (PU_spp_pair_info)
     }
@@ -171,20 +173,6 @@ test_gen_single_bdprob_COR <- function (parameters)
 
 #===============================================================================
 
-test_gen_single_bdprob <- function ()
-    {
-    parameters = init_test_parameters ()
-
-    test_compute_various_link_cts ()
-    test_create_Xu_problem_from_scratch_given_params (parameters)
-    test_create_Xu_problem_from_scratch_not_using_4_Xu_metaparams (parameters)
-    test_create_allowable_size_Xu_problem_from_scratch (parameters)
-    test_gen_single_bdprob_COR_from_scratch_or_Xu_bench_file (parameters)
-    test_gen_single_bdprob_COR (parameters)
-    }
-
-#===============================================================================
-
 test_that("gen_single_bdprob: compute_various_link_cts", {
 
     link_cts_list = test_compute_various_link_cts ()
@@ -200,31 +188,165 @@ test_that("gen_single_bdprob: compute_various_link_cts", {
 
 #-----------------------------------
 
-test_that("gen_single_bdprob: test_create_Xu_problem_from_scratch_given_params", {
+set_correct_values_for_test_1 <- function ()
+    {
+    cor_PU_IDs = c(1,2,1,3,2,3,4,5,4,6,5,6,2,6,3,5,3,5,3,5)
+    cor_spp_IDs = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10)
+    cor_PU_spp_pair_indices = data.frame (PU_ID  = cor_PU_IDs,
+                                          spp_ID = cor_spp_IDs)
 
-    parameters = init_test_parameters ()
-    PU_spp_pair_info = test_create_Xu_problem_from_scratch_given_params (parameters)
+    cor_dependent_node_IDs = c (2,3,5,6)
+    cor_node_IDs = c(1,2,3,4,5,6)
+    cor_group_IDs = c(1,1,1,2,2,2)
+    cor_dependent_set_members = c(FALSE, TRUE, TRUE,
+                                  FALSE, TRUE, TRUE)
+    cor_nodes = data.frame (node_ID  = cor_node_IDs,
+                            group_ID = cor_group_IDs,
+                            dependent_set_member =
+                            cor_dependent_set_members)
+
+    cor_PU_costs = rep (1, 6)
+
+    return (list (
+                cor_PU_IDs = cor_PU_IDs,
+                cor_spp_IDs = cor_spp_IDs,
+                cor_PU_spp_pair_indices = cor_PU_spp_pair_indices,
+                cor_dependent_node_IDs = cor_dependent_node_IDs,
+                cor_node_IDs = cor_node_IDs,
+                cor_group_IDs = cor_group_IDs,
+                cor_dependent_set_members = cor_dependent_set_members,
+                cor_nodes = cor_nodes,
+                cor_PU_costs = cor_PU_costs
+                ))
+    }
+
+#-----------------------------------
+
+testsFor_PU_spp_pair_info_1 <- function (PU_spp_pair_info)
+    {
+    correct_values = set_correct_values_for_test_1 ()
 
     expect_false (is.null (PU_spp_pair_info))
 
-    # expect_equal (3,  PU_spp_pair_info$num_links_within_one_group)
-    # expect_equal (6,  PU_spp_pair_info$tot_num_links_inside_groups)
-    # expect_equal (4,  PU_spp_pair_info$max_possible_num_links_between_groups)
-    # expect_equal (10, PU_spp_pair_info$max_possible_tot_num_links)
-    # expect_equal (20, PU_spp_pair_info$max_possible_tot_num_node_link_pairs)
+    expect_equal (6,  PU_spp_pair_info@num_PUs)
+    expect_equal (10, PU_spp_pair_info@num_spp)
+    expect_equal (4,  PU_spp_pair_info@correct_solution_cost)
+    expect_equal ("PU_ID",  PU_spp_pair_info@PU_col_name)
+    expect_equal ("spp_ID",  PU_spp_pair_info@spp_col_name)
+    expect_true (PU_spp_pair_info@correct_solution_vector_is_known)
+    expect_true (PU_spp_pair_info@prob_generator_params_known)
+
+    expect_equal (correct_values$cor_PU_spp_pair_indices,
+                  PU_spp_pair_info@PU_spp_pair_indices)
+
+    expect_equal (correct_values$cor_dependent_node_IDs,
+                  PU_spp_pair_info@dependent_node_IDs)
+
+    expect_equal (correct_values$cor_nodes,
+                  PU_spp_pair_info@nodes)
+
+    expect_equal (correct_values$cor_PU_costs,
+                  PU_spp_pair_info@PU_costs)
+    }
+
+#-----------------------------------
+
+testsFor_Xu_bdprob_cor_1 <- function (Xu_bdprob_cor)
+    {
+    correct_values = set_correct_values_for_test_1 ()
+
+    expect_false (is.null (Xu_bdprob_cor))
+
+    expect_equal (6,  Xu_bdprob_cor@num_PUs)
+    expect_equal (10, Xu_bdprob_cor@num_spp)
+    expect_equal (4,  Xu_bdprob_cor@correct_solution_cost)
+    expect_equal ("PU_ID",  Xu_bdprob_cor@PU_col_name)
+    expect_equal ("spp_ID",  Xu_bdprob_cor@spp_col_name)
+    expect_true (Xu_bdprob_cor@correct_solution_vector_is_known)
+    expect_true (Xu_bdprob_cor@prob_generator_params_known)
+
+    expect_equal (correct_values$cor_PU_spp_pair_indices,
+                  Xu_bdprob_cor@PU_spp_pair_indices)
+
+    expect_equal (correct_values$cor_dependent_node_IDs,
+                  Xu_bdprob_cor@dependent_node_IDs)
+
+    expect_equal (correct_values$cor_nodes,
+                  Xu_bdprob_cor@nodes)
+
+    expect_equal (correct_values$cor_PU_costs,
+                  Xu_bdprob_cor@PU_costs)
+    }
+
+#-----------------------------------
+#-----------------------------------
+
+test_that("gen_single_bdprob: test_create_Xu_problem_from_scratch_given_params", {
+
+    parameters = init_test_parameters_1 ()
+    PU_spp_pair_info = test_create_Xu_problem_from_scratch_given_params (parameters)
+
+    testsFor_PU_spp_pair_info_1 (PU_spp_pair_info)
+
     })
 
 #-----------------------------------
 
-test_that("gen_single_bdprob: duplicate link failure", {
+test_that("gen_single_bdprob: test_create_Xu_problem_from_scratch_not_using_4_Xu_metaparams", {
 
-    parameters = init_test_parameters ()
-    parameters$duplicate_links_allowed = FALSE
+    parameters = init_test_parameters_1 ()
+    PU_spp_pair_info = test_create_Xu_problem_from_scratch_not_using_4_Xu_metaparams (parameters)
 
-    expect_error (test_create_Xu_problem_from_scratch_given_params (parameters),
-                   "Can't set both use_given_time_as_limit and use_marxan_time_as_limit to TRUE.",
-                  fixed=TRUE)
+    testsFor_PU_spp_pair_info_1 (PU_spp_pair_info)
+
     })
+
+#-----------------------------------
+
+test_that("gen_single_bdprob: test_create_allowable_size_Xu_problem_from_scratch", {
+
+    parameters = init_test_parameters_1 ()
+    PU_spp_pair_info = test_create_allowable_size_Xu_problem_from_scratch (parameters)
+
+    testsFor_PU_spp_pair_info_1 (PU_spp_pair_info)
+
+    })
+
+#-----------------------------------
+
+test_that("gen_single_bdprob: test_gen_single_bdprob_COR_from_scratch_or_Xu_bench_file", {
+
+    parameters = init_test_parameters_1 ()
+    Xu_bdprob_cor = test_gen_single_bdprob_COR_from_scratch_or_Xu_bench_file (parameters)
+
+    testsFor_Xu_bdprob_cor_1 (Xu_bdprob_cor)
+
+    })
+
+#-----------------------------------
+
+test_that("gen_single_bdprob: test_gen_single_bdprob_COR", {
+
+    parameters = init_test_parameters_1 ()
+    Xu_bdprob_cor = test_gen_single_bdprob_COR (parameters)
+
+    testsFor_Xu_bdprob_cor_1 (Xu_bdprob_cor)
+
+    })
+
+#===============================================================================
+
+# test_gen_single_bdprob <- function ()
+#     {
+#     parameters = init_test_parameters_1 ()
+#
+#     test_compute_various_link_cts ()
+#     test_create_Xu_problem_from_scratch_given_params (parameters)
+#     test_create_Xu_problem_from_scratch_not_using_4_Xu_metaparams (parameters)
+#     test_create_allowable_size_Xu_problem_from_scratch (parameters)
+#     test_gen_single_bdprob_COR_from_scratch_or_Xu_bench_file (parameters)
+#     test_gen_single_bdprob_COR (parameters)
+#     }
 
 #===============================================================================
 
