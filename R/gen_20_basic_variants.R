737b74cd2e213a00142cb51965de77ce340c0023
Bill Langford <bill.langford@rmit.edu.au>
2019-04-05 14:14:52 +1100

    Add function to gen 4 simple random variants
    This code is different from the original code to generate 4 basic variants
    in that its main purpose is to generate a small, simple set of problems
    to use in the first paper where it's explaining and demonstrating the
    problem generator.  The problems generated here have 1 COR Base, 1 COR Wrap,
    1 APP Wrap with random, unmatched FN, FP, and no cost error, and 1 APP Wrap
    with random, unmatched FN, FP, and WITH cost error.

--- R/gen_20_basic_variants.R
+++ R/gen_20_basic_variants.R
@@ -550,7 +550,7 @@ cat (  "\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 #' Create 1 correct problem and 1 wrap of that correct problem, then from the
 #' Wrap problem, create 4 apparent variants using the following errors: FP only,
 #' FN only, FP and FN, and finally, FP and FN with matching counts.  Based on
-#' each of these 6 variants (2 correct and 4 apparent), then
+#' each of the 5 app variants (1 correct and 4 apparent), then
 #' create another apparent version of each problem that includes cost error too.
 #' This gives a total of 11 problems.
 #'
@@ -724,6 +724,197 @@ cat (  "\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         }  #  end if - wrap_lognormal_dist_around_Xu
 
     }  #  end function - gen_11_variants_mixing_FP_FN_and_cost_error()
+
+#===============================================================================
+
+#' Generate 4 simple random variants of a problem
+#'
+#' Create 1 correct problem and 1 wrap of that correct problem, then from the
+#' Wrap problem, create 1 apparent variant using FP and FN error but no cost
+#' error.  Then create another apparent using FP and FN, this time WITH cost
+#' error.  This gives a total of 4 problems.
+#'
+#-------------------------------------------------------------------------------
+
+#' @inheritParams std_param_defns
+#' @param err_amt numeric amount of error to add in creating apparent problems
+#'
+#' @return Returns nothing.
+#' @export
+
+#-------------------------------------------------------------------------------
+
+gen_4_simple_random_variants_mixing_FP_FN_and_cost_error <- function (parameters,
+                                                         integerize)
+    {
+                    cat ("\n\nAT START OF gen_4_simple_random_variants_mixing_FP_FN_and_cost_error().\n\n")
+
+    #===============================================================================
+    #            Determine amount of error to add in apparent problems.
+    #===============================================================================
+
+        #  Set FP error amount.
+
+    default_FP_err_amt_lower_bound = 0
+    FP_err_amt_lower_bound = vn (parameters$FP_err_amt_lower_bound,
+                                 range_lo = 0,
+                                 bounds_types = "ii",
+                                 def_on_empty = TRUE,
+                                 def = default_FP_err_amt_lower_bound,
+                                 treat_NULL_as_empty = TRUE,
+                                 treat_NA_as_empty = FALSE)
+
+    default_FP_err_amt_upper_bound = 0.10
+    FP_err_amt_upper_bound = vn (parameters$FP_err_amt_upper_bound,
+                                  range_lo = FP_err_amt_lower_bound,
+                                  bounds_types = "ii",
+                                  def_on_empty = TRUE,
+                                  def = default_FP_err_amt_upper_bound,
+                                  treat_NULL_as_empty = TRUE,
+                                  treat_NA_as_empty = FALSE)
+
+    FP_err_amt = runif (1, FP_err_amt_lower_bound, FP_err_amt_upper_bound)
+
+    #----------
+
+        #  Set FN error amount.
+
+    default_FN_err_amt_lower_bound = 0
+    FN_err_amt_lower_bound = vn (parameters$FN_err_amt_lower_bound,
+                                 range_lo = 0,
+                                 bounds_types = "ii",
+                                 def_on_empty = TRUE,
+                                 def = default_FN_err_amt_lower_bound,
+                                 treat_NULL_as_empty = TRUE,
+                                 treat_NA_as_empty = FALSE)
+
+    default_FN_err_amt_upper_bound = 0.10
+    FN_err_amt_upper_bound = vn (parameters$FN_err_amt_upper_bound,
+                                  range_lo = FN_err_amt_lower_bound,
+                                  bounds_types = "ii",
+                                  def_on_empty = TRUE,
+                                  def = default_FN_err_amt_upper_bound,
+                                  treat_NULL_as_empty = TRUE,
+                                  treat_NA_as_empty = FALSE)
+
+    FN_err_amt = runif (1, FN_err_amt_lower_bound, FN_err_amt_upper_bound)
+
+    #----------
+
+        #  Set cost error amount.
+
+    default_cost_err_amt_lower_bound = 0
+    cost_err_amt_lower_bound = vn (parameters$cost_err_amt_lower_bound,
+                                   range_lo = 0,
+                                   bounds_types = "ii",
+                                   def_on_empty = TRUE,
+                                   def = default_cost_err_amt_lower_bound,
+                                   treat_NULL_as_empty = TRUE,
+                                   treat_NA_as_empty = FALSE)
+
+    default_cost_err_amt_upper_bound = 0.10
+    cost_err_amt_upper_bound = vn (parameters$cost_err_amt_upper_bound,
+                                   range_lo = cost_err_amt_lower_bound,
+                                   bounds_types = "ii",
+                                   def_on_empty = TRUE,
+                                   def = default_cost_err_amt_upper_bound,
+                                   treat_NULL_as_empty = TRUE,
+                                   treat_NA_as_empty = FALSE)
+
+    cost_err_amt = runif (1, cost_err_amt_lower_bound, cost_err_amt_upper_bound)
+
+    #===============================================================================
+    #       Generate a base problem, i.e, create the Xu graph nodes and edge_list.
+    #===============================================================================
+
+    base_COR_bd_prob = gen_single_bdprob_COR (parameters,
+                                              integerize,
+                                              base_prob_name_stem = "base_prob",
+                                              cor_dir_name_stem = "cor"
+                                              )
+
+                    cat ("\n\n-----  base_COR_bd_prob@UUID = '", base_COR_bd_prob@UUID,
+                         "', checksum = '", base_COR_bd_prob@checksum, "'  -----\n\n")
+
+    do_rs_analysis_and_output (base_COR_bd_prob,
+                               base_COR_bd_prob,
+                               parameters)
+
+                    cat("\n\njust after do_rs_analysis_and_output() for Base COR problem")
+                    cat ("\n\n================================================================================")
+                    cat ("\n================================================================================\n\n")
+
+cat ("\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+cat ("\n---  Just after all generation for BASE problem, about to do WRAP problems  ---")
+cat (  "\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n\n")
+
+    #===============================================================================
+    #       Generate a wrapped problem around the BASE problem.
+    #===============================================================================
+
+    if (value_or_FALSE_if_null (parameters$wrap_lognormal_dist_around_Xu) &
+        ! value_or_FALSE_if_null (parameters$read_Xu_problem_from_Xu_bench_file))
+        {
+        wrapped_COR_bd_prob =
+#            gen_bdprob (parameters,
+            gen_multi_bdprob (parameters,
+                              integerize,
+                              base_COR_bd_prob)
+
+        do_rs_analysis_and_output (wrapped_COR_bd_prob,
+                                   wrapped_COR_bd_prob,
+                                   parameters)
+
+                        cat("\n\njust after do_COR_rs_analysis_and_output() for Wrapped COR problem")
+                        cat ("\n\n================================================================================")
+                        cat ("\n================================================================================\n\n")
+
+            #----------------------------------------------
+            #  FP & FN, counts NOT matched, NO cost error
+            #----------------------------------------------
+
+    gen_1_app_variant (wrapped_COR_bd_prob,
+                                       parameters,
+
+                                       gen_cost_errors = FALSE,
+                                       cost_error_frac_bound = cost_err_amt,
+
+                                       gen_FP_FN_errors = TRUE,
+
+                                       spp_occ_FP_const_rate = FP_err_amt,
+                                       spp_occ_FN_const_rate = FN_err_amt,
+
+                                       match_error_counts = FALSE)
+
+                       cat("\n\njust after gen_1_app_variant() WITHOUT cost error for Wrapped problem")
+                        cat ("\n\n================================================================================")
+                        cat ("\n================================================================================\n\n")
+
+
+           #-------------------------------------------------
+            #  FP & FN, counts NOT matched, WITH cost error
+            #------------------------------------------------
+
+    gen_1_app_variant (wrapped_COR_bd_prob,
+                                       parameters,
+
+                                       gen_cost_errors = TRUE,
+                                       cost_error_frac_bound = cost_err_amt,
+
+                                       gen_FP_FN_errors = TRUE,
+
+                                       spp_occ_FP_const_rate = FP_err_amt,
+                                       spp_occ_FN_const_rate = FN_err_amt,
+
+                                       match_error_counts = FALSE)
+
+                       cat("\n\njust after gen_1_app_variant() WITH cost error for Wrapped problem")
+                        cat ("\n\n================================================================================")
+                        cat ("\n================================================================================\n\n")
+
+        }  #  end if - wrap_lognormal_dist_around_Xu
+
+    }  #  end function - gen_4_simple_random_variants_mixing_FP_FN_and_cost_error()
 
 #===============================================================================
 
